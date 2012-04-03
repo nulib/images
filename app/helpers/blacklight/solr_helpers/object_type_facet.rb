32d5da92789b18fd63ca1914ecdce6caa0e8cf65
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-04-03 14:42:22 -0500

    Fixed object type filter

--- app/helpers/blacklight/solr_helpers/object_type_facet.rb
+++ app/helpers/blacklight/solr_helpers/object_type_facet.rb
@@ -1,14 +1,13 @@
 module Blacklight::SolrHelpers::ObjectTypeFacet
 
   def multiresimage_object_type_facet(solr_parameters, user_parameters)
-    solr_parameters[:q] ||= []
+    solr_parameters[:fq] ||= []
 
         # Only apply this if the search is happening from inside the Hydra app.
         # External searches need Works and Images (Hydra app just needs Images).
     if (self.class.name != "ExternalSearchController")
-      solr_parameters[:q] << 'object_type_facet:Multiresimage'
+      solr_parameters[:fq] << 'object_type_facet:Multiresimage'
     end
-    #solr_parameters[:q] << 'default_search_t:'.params[:q]
   end
 
 end
