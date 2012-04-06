f9179215f40cf68efa90cf8de5430c4ea1638161
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-04-06 12:42:02 -0500

    DIL-206 add link back to main page

--- spec/requests/uploads_spec.rb
+++ spec/requests/uploads_spec.rb
@@ -11,6 +11,8 @@ describe "Uploading files" do
       page.should have_selector("form#fileupload[action='#{uploads_create_path}']")
       page.should have_selector("table > tbody.files")
 
+      page.should have_selector("#sidebar a[href='#{catalog_index_path}']", :text=>'Search')
+
       attach_file('files[]', Rails.root.join("spec/fixtures/images/The_Tilled_Field.jpg"))
       click_on('Start upload')
       # Can't get this to work. even with :js =>true
