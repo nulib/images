31c4a262a813492bd49a570efc20a3b7342e4aa4
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-04-11 13:16:43 -0500

    Extract rights metadata functions from multiresimage

--- app/models/multiresimage.rb
+++ app/models/multiresimage.rb
@@ -3,6 +3,7 @@ class Multiresimage < ActiveFedora::Base
   include Hydra::ModelMethods
   include ActiveFedora::Relationships
   include Rails.application.routes.url_helpers
+  include Dil::RightsMetadata
   
   belongs_to :collection, :class_name=> "DILCollection", :property=> :is_governed_by
 
@@ -47,45 +48,6 @@ class Multiresimage < ActiveFedora::Base
   delegate :file_name, :to=>:properties, :unique=>true
   delegate :related_ids, :to=>:VRA, :at=>[:image, :relationSet, :imageOf, :relation_relids]
 
-  def read_groups
-    rightsMetadata.groups.map {|k, v| k if v == 'read'}.compact
-  end
-
-  # Grant read permissions to the groups specified. Revokes read permission for all other groups.
-  # @param[Array] groups a list of groups
-  # @example
-  #  r.read_groups= ['one', 'two', 'three']
-  #  r.read_groups 
-  #  => ['one', 'two', 'three']
-  #
-  def read_groups=(groups)
-    set_read_groups(groups, read_groups)
-  end
-
-  # Grant read permissions to the groups specified. Revokes read permission for
-  # any of the eligible_groups that are not in groups.
-  # This may be used when different users are responsible for setting different
-  # groups.  Supply the groups the current user is responsible for as the 
-  # 'eligible_groups'
-  # @param[Array] groups a list of groups
-  # @param[Array] eligible_groups the groups that are eligible to have their read permssion revoked. 
-  # @example
-  #  r.read_groups = ['one', 'two', 'three']
-  #  r.read_groups 
-  #  => ['one', 'two', 'three']
-  #  r.set_read_groups(['one'], ['three'])
-  #  r.read_groups
-  #  => ['one', 'two']  ## 'two' was not eligible to be removed
-  #
-  def set_read_groups(groups, eligible_groups)
-    g = rightsMetadata.groups.select {|k, v| v == 'edit'}
-    (eligible_groups - groups).each do |group_name|
-      #Strip permissions from groups not privided
-      g[group_name] = 'none'
-    end
-    groups.each { |name| g[name] = 'read'}
-    rightsMetadata.update_permissions("group"=>g)
-  end
 
   def attach_file(files)
     if files.present?
