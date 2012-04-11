fa0b833f2f3b6eb56372db6953c1b431233e985d
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-04-11 14:04:54 -0500

    Test for collectionController#edit

--- spec/controllers/dil_collections_controller_spec.rb
+++ spec/controllers/dil_collections_controller_spec.rb
@@ -23,4 +23,41 @@ describe DilCollectionsController do
       end
     end
   end
+  describe "#edit" do
+    before do
+      @collection = FactoryGirl.create(:collection)
+    end
+    describe "as a logged in user" do
+      before do
+        @user = FactoryGirl.find_or_create(:archivist)
+        sign_in @user
+      end
+      describe "when I am authorized to edit" do
+        before do
+          @collection.apply_depositor_metadata(@user.uid)
+          @collection.save!
+        end
+        it "should be successful" do
+          get :edit, :id=>@collection.pid
+          response.should be_success
+          assigns[:collection].should be_kind_of DILCollection
+        end
+      end
+      describe "when I am not authorized" do
+        it "should redirect" do
+          get :edit, :id=>@collection.pid
+          response.should redirect_to(root_path)
+          flash[:alert].should == "You are not authorized to access this page."
+        end
+      end
+    end
+    describe "when not logged in" do
+      it "should redirect" do
+        get :edit, :id=>@collection.pid
+        assigns[:dil_collection].should be_nil
+        response.should redirect_to(root_path)
+        flash[:alert].should == "You are not authorized to access this page."
+      end
+    end
+  end
 end
