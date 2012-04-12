0537f08a482d79b59bf7a98199b7cb66aad900fd
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-04-12 13:41:25 -0500

    DIL-213, DIL-238 add and remove users from a group

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
+
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
+  end
+
   # Adds a few additional behaviors into the application controller 
    include Blacklight::Controller  
 # Adds Hydra behaviors into the application controller 
