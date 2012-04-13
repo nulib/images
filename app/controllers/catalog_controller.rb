22afa126fd018c95f47af216c7f27f422c402935
Mike Stroming <mstrom81@gmail.com>
2012-04-13 16:39:22 -0500

    Added search_field_t to qf

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -16,7 +16,7 @@ class CatalogController < ApplicationController
   configure_blacklight do |config|
     config.default_solr_params = { 
       :qt => 'search',
-      :qf => 'title_display_t', # List all the fields you want to search here
+      :qf => 'title_display_t,search_field_t', # List all the fields you want to search here
       :rows => 10 
     }
 
