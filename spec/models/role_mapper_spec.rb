ecd1de376bb3a3f2db8d7b77d35496021b3b003b
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-04-13 15:22:24 -0500

    Comment out the eduPersonAffiliation.  We can't just append strings to a list of objects

---
+++ spec/models/role_mapper_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe User do
+  before do
+    Group.any_instance.stub :persist_to_ldap
+    @user = FactoryGirl.find_or_create(:archivist)
+    @g1 = FactoryGirl.create(:user_group, :users=>[@user.uid], :owner=>FactoryGirl.create(:user))
+    Dil::LDAP.stub(:groups_for_user).with(@user.uid).and_return([@g1.code])
+  end
+  subject {::RoleMapper.roles(@user.uid)}
+  it { should == [@g1.code]}
+end
