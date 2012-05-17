5bd6cf7c914075a568066b5621a4aa6866c162d8
root <root@cecil.library.northwestern.edu>
2012-05-17 00:48:59 -0500

    Added delegates

--- app/models/vrawork.rb
+++ app/models/vrawork.rb
@@ -23,12 +23,12 @@ class Vrawork  < ActiveFedora::Base
     m.field 'depositor', :string
   end
 
-  delegate :titleSet_display, :to=>:VRA, :unique=>true
-  delegate :agentSet_display, :to=>:VRA, :unique=>true
-  delegate :dateSet_display, :to=>:VRA, :unique=>true
-  delegate :descriptionSet_display, :to=>:VRA, :unique=>true
-  delegate :subjectSet_display, :to=>:VRA, :unique=>true
-  delegate :culturalContextSet_display, :to=>:VRA, :unique=>true
+  delegate :titleSet_display_work, :to=>:VRA, :unique=>true
+  delegate :agentSet_display_work, :to=>:VRA, :unique=>true
+  delegate :dateSet_display_work, :to=>:VRA, :unique=>true
+  delegate :descriptionSet_display_work, :to=>:VRA, :unique=>true
+  delegate :subjectSet_display_work, :to=>:VRA, :unique=>true
+  delegate :culturalContextSet_display_work, :to=>:VRA, :unique=>true
   #delegate :ref_id, :to=>:VRA, :unique=>true
 
   #def initialize( attrs={} )
@@ -45,12 +45,14 @@ class Vrawork  < ActiveFedora::Base
   end
   
   def update_ref_id(ref_id)
+    #Refactor to use proxy/delegates
     node_set = self.datastreams["VRA"].ng_xml.xpath('/vra:vra/vra:work[@refid]')
     node_set[0].set_attribute("refid", ref_id)
     #self.save!
   end
   
   def update_relation_set(image_pid)
+    #Refactor to use proxy/delegates
     node_set = self.datastreams["VRA"].ng_xml.xpath('/vra:vra/vra:work/vra:relationSet/vra:relation')
     node_set[0].set_attribute("pref", "true")
     node_set[0].set_attribute("relids", image_pid)
@@ -58,9 +60,13 @@ class Vrawork  < ActiveFedora::Base
 	#self.save!
   end
   
-   def update_agent_set(agent_set_display)
-    node_set = self.datastreams["VRA"].ng_xml.xpath('/vra:vra/vra:work/vra:agentSet/vra:display')
-    logger.debug("NODESET:" + node_set.to_s)
-   end
-  
+   #def update_agent_set(agent_set_display)
+    #self.agentSet_display_work = agent_set_display
+   
+    #node_set = self.datastreams["VRA"].ng_xml.xpath('/vra:vra/vra:work/vra:agentSet/vra:display')  
+    #logger.debug("NODESET TEST: " + node_set.to_xml)
+    #self.datastreams["VRA"].agentSet_display = node_set[0].content
+    #Not sure why this doesn't work
+    #self.datastreams["VRA"].agentSet_display = agent_set_display
+  # end
 end
