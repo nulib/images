7fb4caac1a30c814eeabcdcc1b7fa6f043b696d2
root <root@cecil.library.northwestern.edu>
2012-05-24 14:22:52 -0500

    Edit for images

--- app/controllers/dil_collections_controller.rb
+++ app/controllers/dil_collections_controller.rb
@@ -1,6 +1,7 @@
 class DilCollectionsController < ApplicationController
   include Hydra::AssetsControllerHelper
-
+  require 'stomp'
+  
   def create
     authorize!(:create, DILCollection)
 		@dil_collection = DILCollection.new()
@@ -73,13 +74,31 @@ class DilCollectionsController < ApplicationController
   def export
     @collection = DILCollection.find(params[:id])
     authorize! :update, @collection
-    read_groups = params[:dil_collection].delete(:read_groups)
-    if read_groups.present?
-      eligible = current_user.owned_groups.map(&:code)
-      @collection.set_read_groups(read_groups, current_user.owned_groups.map(&:code))
-    end
-    export_xml = @collection.export_pids_as_xml
+    #read_groups = params[:dil_collection].delete(:read_groups)
+    #if read_groups.present?
+      #eligible = current_user.owned_groups.map(&:code)
+      #@collection.set_read_groups(read_groups, current_user.owned_groups.map(&:code))
+    #end
     
+    export_xml = @collection.export_image_urls_as_xml
+    export_xml << current_user.email
+    logger.debug("export_xml: " + export_xml)
+    # call CGI script with file location (path, name and id)
+    # CGI on gandalf will pull file from shirley
+    cgi_url = "http://gandalf.library.northwestern.edu/cgi-bin/hydra/hydra-export-ppt-jms.cgi"
+	#logger.debug("cgi url: " + cgi_url)
+	# response will be status of script that puts message in queue
+	logger.debug("Before CGI call for export")
+	#post_args = {'xml' => export_xml}
+	cgi_response = Net::HTTP.post_form(URI.parse(cgi_url), 'collection_xml' => export_xml).body
+	#cgi_response = Net::HTTP.get_response(URI.parse(cgi_url)).body
+	logger.debug("After CGI call for export")
+	logger.debug("response:" + cgi_response)
+    
+    #client = Stomp::Client.open "stomp://gandalf.library.northwestern.edu:61613"
+    #client = Stomp::Client.new("", "", "stomp://129.105.203.30", 61613)
+    #client.send("/queue/hydra.export", export_xml)
+
     flash[:notice] = "Saved changes to #{@collection.title}"
     
     redirect_to edit_dil_collection_path(@collection)
