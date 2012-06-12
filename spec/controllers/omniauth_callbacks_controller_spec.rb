d9d7feb6e82942c64a6fdae148c30c3561543585
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-06-12 11:37:35 -0500

    Fixed omniauth_callback_controller_spec

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -4,6 +4,20 @@ describe Users::OmniauthCallbacksController do
   include Devise::TestHelpers
 
   before do
+    OmniAuth.config.mock_auth[:ldap] = {
+      :provider => 'ldap',
+      :uid=> "uid=vanessa,ou=people,dc=example,dc=com",
+      :info => {
+          :name => 'Vanessa User',
+          :email => 'vanessa@example.com',
+          :nickname =>  'vanessa'
+      },
+      :extra => {
+        :raw_info => {
+            :edupersonaffiliation => ["staff", "student"]
+        }
+      }
+    }
     request.env["devise.mapping"] = Devise.mappings[:user]
     request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:ldap]
   end
