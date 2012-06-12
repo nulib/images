0dbf11e3a6b91c064d9fd8bf2db90db298ab8714
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-06-12 12:15:45 -0500

    Fixed uploads_controller_spec

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -113,42 +113,8 @@ class UploadsController < ApplicationController
   def enqueue
     
     current_user.upload_files.each do |file|
-      #create file on server from Fedora object datastream
-      new_filepath="/usr/local/rails_uploaded_images/" << file.pid.gsub(":","") << ".jpg"
-      
-      Net::HTTP.start("127.0.0.1", 8983) { |http|
-        resp = http.get("/fedora/objects/" + file.pid + "/datastreams/raw/content")
-        logger.debug("response:" + resp.to_s) 
-        open(new_filepath ,"wb") { |new_file|
-          new_file.write(resp.body)
-        }
-      }
-      
-      FileUtils.chmod(0755, new_filepath)
-      
       @image_processing_request = ImageProcessingRequest.create!(:status => 'NEW', :pid=>file.pid, :email => 'm-stroming@northwestern.edu')
-      #@image_processing_request.enqueue
-      
-      # call CGI script with file location (path, name and id)
-      # CGI on gandalf will pull file from shirley
-      cgi_url = "http://gandalf.library.northwestern.edu/cgi-bin/hydra/hydra-jms.cgi?image_path=" + new_filepath + "&request_id=" + @image_processing_request.id.to_s
-	  logger.debug("cgi url: " + cgi_url)
-	  # response will be status of script that puts JMS message in queue
-	  logger.debug("Before CGI call")
-	  cgi_response = Net::HTTP.get_response(URI.parse(cgi_url)).body
-	  logger.debug("After CGI call")
-	  logger.debug("response:" + cgi_response)
-	 
-	  #cgi_response = nil
-	  if(!cgi_response.nil?)
-	   status = "JMS" + cgi_response
-	   logger.debug("Update status to: " + status)
-	   #update status column in table
-	   @image_processing_request.update_attributes(:status=>status)
-	  else
-       logger.debug("cgi_response is null")
-       #update_status
-      end
+      @image_processing_request.enqueue
       
     end
     
