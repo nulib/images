88be1946fc22a369339af755f9bab0e8fc8b92ed
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-06-12 14:36:37 -0500

    Clean up comments

--- app/models/vrawork.rb
+++ app/models/vrawork.rb
@@ -9,19 +9,12 @@ class Vrawork  < ActiveFedora::Base
   # Uses the Hydra Rights Metadata Schema for tracking access permissions & copyright
   has_metadata :name => "rightsMetadata", :type => Hydra::Datastream::RightsMetadata 
   
-  # Uses the Hydra MODS Article profile for tracking most of the descriptive metadata
-  #has_metadata :name => "MODS", :type => ModsArticleDatastream
-
-  # Uses the VRA profile for tracking some of the descriptive metadata
-#	has_metadata :name => "VRA", :type => Hydra::VRAWorkDatastream 
+  # Uses the VRA profile for tracking the descriptive metadata
    has_metadata :name => "VRA", :type => VRADatastream
 
 
   # A place to put extra metadata values
-  has_metadata :name => "properties", :type => ActiveFedora::MetadataDatastream do |m|
-    m.field 'collection', :string
-    m.field 'depositor', :string
-  end
+  has_metadata :name => "properties", :type => Hydra::Datastream::Properties
 
   delegate :titleSet_display_work, :to=>:VRA, :unique=>true
   delegate :agentSet_display_work, :to=>:VRA, :unique=>true
