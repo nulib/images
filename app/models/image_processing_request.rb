ba8b757265db67ddaa0b2fd325a089ef54214c6a
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-06-12 12:15:45 -0500

    Fixed uploads_controller_spec

--- app/models/image_processing_request.rb
+++ app/models/image_processing_request.rb
@@ -20,26 +20,43 @@ class ImageProcessingRequest < ActiveRecord::Base
       return
     end
     new_filepath = image.write_out_raw
+    #create file on server from Fedora object datastream
+
+    # new_filepath= File.join(DIL::Application.config.processing_file_path, "#{file.pid.gsub(":","")}.jpg")
+    # 
+    # Net::HTTP.start("127.0.0.1", 8983) { |http|
+    #   resp = http.get("/fedora/objects/" + file.pid + "/datastreams/raw/content")
+    #   logger.debug("response:" + resp.to_s) 
+    #   open(new_filepath ,"wb") { |new_file|
+    #     new_file.write(resp.body)
+    #   }
+    # }
+    # 
+    # FileUtils.chmod(0755, new_filepath)
       
+
+
     # TODO Can we replace the cgi-bin with stomp?
     # call CGI script with file location (path, name and id)
-    # CGI on msg server will pull file from app server
-    cgi_url = "http://www.example.com/cgi-bin/hydra/hydra-jms.cgi?image_path=" + new_filepath + "&request_id=#{id}"
-    logger.debug("cgi url: " + cgi_url)
-    # response will be status of script that puts JMS message in queue
-    logger.debug("Before CGI call")
-    cgi_response = Net::HTTP.get_response(URI.parse(cgi_url)).body
-    logger.debug("After CGI call")
-    #logger.debug("response:" + cgi_response)
-   
-    cgi_response = nil
-    if(!cgi_response.nil?)
-      status = "JMS" + cgi_response
-      logger.debug("Update status to: " + status)
-      #update status column in table
-      update_attribute(:status, status)
-    else
+    # CGI on gandalf will pull file from shirley
+    cgi_url = "http://gandalf.library.northwestern.edu/cgi-bin/hydra/hydra-jms.cgi?image_path=" + new_filepath + "&request_id=" + @image_processing_request.id.to_s
+	  logger.debug("cgi url: " + cgi_url)
+	  # response will be status of script that puts JMS message in queue
+	  logger.debug("Before CGI call")
+	  cgi_response = Net::HTTP.get_response(URI.parse(cgi_url)).body
+	  logger.debug("After CGI call")
+	  logger.debug("response:" + cgi_response)
+	 
+	  #cgi_response = nil
+	  if cgi_response.nil?
       logger.debug("cgi_response is null")
+	  else
+	    status = "JMS" + cgi_response
+	    logger.debug("Update status to: " + status)
+	    #update status column in table
+      update_attribute(:status, status)
     end
+    
+
   end
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,4 +32,6 @@ DIL::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  config.processing_file_path = "tmp"
 end
