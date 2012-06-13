0e9215d560e0c0f2bb5da173d429b9948d2c44f5
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-06-12 21:24:45 -0500

    Removed duplicate code

--- app/helpers/object_type_facet.rb
+++
@@ -1,9 +0,0 @@
-# -*- encoding : utf-8 -*-
-module Blacklight::SolrHelpers::ObjectTypeFacet
-
-  def multiresimage_object_type_facet(solr_parameters, user_parameters)
-    solr_parameters[:q] ||= []
-    solr_parameters[:q] << 'object_type_facet:Multiresimage'
-  end
-
-end
