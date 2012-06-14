b0ab71ffb119a32e40d213e705e67a308ec74f31
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-06-14 16:51:54 -0500

    Added batch updating

---
+++ spec/routing/batch_update_routes_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe "Routes for batch_update" do
+  it "should route index" do 
+    { :get => '/batch_updates' }.should route_to( :controller => "batch_updates", :action => "index")
+  end
+  it "should route edit" do 
+    { :get => edit_batch_updates_path }.should route_to( :controller => "batch_updates", :action => "edit")
+  end
+  it "should route update" do 
+    { :put => batch_updates_path }.should route_to( :controller => "batch_updates", :action => "update")
+  end
+  it "should route add" do 
+    { :put => add_batch_update_path(7)}.should route_to( :controller => "batch_updates", :action => "add", :id=>'7')
+  end
+  it "should route delete" do 
+    { :delete => batch_update_path(7) }.should route_to( :controller => "batch_updates", :action => "destroy", :id=>'7')
+  end
+
+
+end
