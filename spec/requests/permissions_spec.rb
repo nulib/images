181477a622bfd9923eb962ce1a894ff15bca2caa
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2012-06-18 16:54:11 -0500

    switching permissions form to work within regular metadata edit form (single-field submit will be all-javascript

--- spec/requests/permissions_spec.rb
+++ spec/requests/permissions_spec.rb
@@ -16,12 +16,12 @@ describe "Edit permissions for a multiresimage" do
       page.should_not have_selector("#multiresimage_permissions_group_test_group_1")
       fill_in("multiresimage_permissions_new_group_name", :with=>"test_group_1")
       select("View", :from=>"multiresimage_permissions_new_group_permission")
-      click_on('Add Group')
-      page.should have_selector(".msg.notice", :text=>"Updated permissions for #{@fixture.titleSet_display}.")
+      click_on('Save changes')
+      page.should have_selector(".msg.notice", :text=>"Saved changes to #{@fixture.pid}")
       page.should have_select("multiresimage_permissions_group_test_group_1", :selected => "View")
       select("Edit", :from=>"multiresimage_permissions_group_test_group_1")
-      click_on('test_group_1_update_permission')
-      page.should have_selector(".msg.notice", :text=>"Updated permissions for #{@fixture.titleSet_display}.")
+      click_on('Save changes')
+      page.should have_selector(".msg.notice", :text=>"Saved changes to #{@fixture.pid}")
       page.should have_select("multiresimage_permissions_group_test_group_1", :selected => "Edit")
     end
     
@@ -31,12 +31,12 @@ describe "Edit permissions for a multiresimage" do
       page.should_not have_selector("#multiresimage_permissions_user_test_user_1")
       fill_in("multiresimage_permissions_new_user_name", :with=>"test_user_1")
       select("View", :from=>"multiresimage_permissions_new_user_permission")
-      click_on('Add Person')
-      page.should have_selector(".msg.notice", :text=>"Updated permissions for #{@fixture.titleSet_display}.")
+      click_on('Save changes')
+      page.should have_selector(".msg.notice", :text=>"Saved changes to #{@fixture.pid}")
       page.should have_select("multiresimage_permissions_user_test_user_1", :selected => "View")
       select("Discover", :from=>"multiresimage_permissions_user_test_user_1")
-      click_on('test_user_1_update_permission')
-      page.should have_selector(".msg.notice", :text=>"Updated permissions for #{@fixture.titleSet_display}.")
+      click_on('Save changes')
+      page.should have_selector(".msg.notice", :text=>"Saved changes to #{@fixture.pid}")
       page.should have_select("multiresimage_permissions_user_test_user_1", :selected => "Discover")
     end
     
@@ -45,8 +45,8 @@ describe "Edit permissions for a multiresimage" do
       page.should have_selector("fieldset.permissions")
       page.should have_selector("#multiresimage_permissions_user_test_user_1")
       select("No Access", :from=>"multiresimage_permissions_user_test_user_1")
-      click_on('test_user_1_update_permission')
-      page.should have_selector(".msg.notice", :text=>"Updated permissions for #{@fixture.titleSet_display}.")
+      click_on('Save changes')
+      page.should have_selector(".msg.notice", :text=>"Saved changes to #{@fixture.pid}")
       page.should_not have_selector("#multiresimage_permissions_user_test_user_1")
     end
   end
