e02f01bf333cc9f885f65a22328b3f728c645ef8
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2012-06-20 14:07:38 -0500

    better coverage for permissions spec

--- spec/helpers/permissions_helper_spec.rb
+++ spec/helpers/permissions_helper_spec.rb
@@ -7,4 +7,19 @@ describe PermissionsHelper do
       sort_permissions(permissions_array).should == [{:type=>"user", :access=>"read", :name=>"auser5566"}, {:type=>"user", :access=>"edit", :name=>"vanessa"}, {:type=>"group", :access=>"discover", :name=>"xstudents"}]
     end
   end
+  describe "permissions_users" do
+    it "should sort the permissions by name (user/group id)" do
+      permissions_array = [{:type=>"group", :access=>"discover", :name=>"xstudents"}, {:type=>"user", :access=>"read", :name=>"auser5566"}, {:type=>"user", :access=>"edit", :name=>"vanessa"}]
+      obj = stub("Object", :permissions=>permissions_array)
+      permissions_users(obj).should == [{:type=>"user", :access=>"read", :name=>"auser5566"}, {:type=>"user", :access=>"edit", :name=>"vanessa"}]
+    end
+  end
+  describe "permissions_groups" do
+    it "should sort the permissions by name (user/group id)" do
+      permissions_array = [{:type=>"group", :access=>"discover", :name=>"xstudents"}, {:type=>"user", :access=>"read", :name=>"auser5566"}, {:type=>"user", :access=>"edit", :name=>"vanessa"}]
+      obj = stub("Object", :permissions=>permissions_array)
+      permissions_groups(obj).should == [{:type=>"group", :access=>"discover", :name=>"xstudents"}]
+    end
+  end
+  
 end
