609ecd9af90d28567aa4e4a6b1950ff621631ffe
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2012-06-20 14:04:56 -0500

    permissions - separating users & groups, sorting alphabetically

--- app/helpers/permissions_helper.rb
+++ app/helpers/permissions_helper.rb
@@ -1,5 +1,23 @@
 module PermissionsHelper
-  def permissions_groups_options
-    
+  
+  def permissions_users(obj)
+    sort_permissions(obj.permissions.select {|p| p[:type] == "user"})
+  end
+  
+  def permissions_groups(obj)
+    sort_permissions(obj.permissions.select {|p| p[:type] == "group"})    
+  end
+  
+  def sort_permissions(permissions)
+    permissions.sort_by! {|p| p[:name] }
+  end
+  
+  
+  def hashify_permissions(permissions)
+    perms_hash = {}
+    permissions.each do |p|
+      perms_hash[p[:name]] = p[:access]
+    end
+    return perms_hash
   end
 end
