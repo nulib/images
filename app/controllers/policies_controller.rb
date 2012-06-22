8e1b83cf184e68cb00620664186053862cc1c9a7
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-06-22 16:16:09 -0500

    Added permissions to policies formw

--- app/controllers/policies_controller.rb
+++ app/controllers/policies_controller.rb
@@ -2,8 +2,10 @@ class PoliciesController < ApplicationController
   load_and_authorize_resource :class=>AdminPolicy
 
   def new
+    @page_title = 'Create a policy'
   end
 
   def edit
+    @page_title = 'Edit policy'
   end
 end
