57827fdbd7aed73e1934555e48c94faa837d2be2
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2012-06-22 15:57:22 -0500

    making spec helper nuke more gently

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -1,5 +1,173 @@
 require 'spec_helper'
 
+describe "given a Faculty-created image with no custom access set" do
+  before do
+    @image = Multiresimage.find("inu:dil-default-access-image")
+  end
+  context "someone with NU id" do
+    before do
+      @user = FactoryGirl.find_or_create(:nu_id_holder)
+      Group.any_instance.stub :persist_to_ldap
+    end
+    subject { Ability.new(@user) }
+    it "should not be able to discover the image" do
+      subject.can?(:discover, @image).should be_false
+    end
+    it "should not be able to view the image" do
+      subject.can?(:read, @image).should be_false
+    end
+    it "should not be able to edit, update and destroy the image" do
+      subject.can?(:edit, @image).should be_false
+      subject.can?(:update, @image).should be_false
+      subject.can?(:destroy, @image).should be_false
+    end
+  end
+  context "the Creator" do
+    before do
+      @user = FactoryGirl.find_or_create(:joe_creator)
+      Group.any_instance.stub :persist_to_ldap
+    end
+    subject { Ability.new(@user) }
+    it "should be able to discover the image" do
+      subject.can?(:discover, @image).should be_true
+    end
+    it "should be able to view the image" do
+      subject.can?(:read, @image).should be_true
+    end
+    it "should be able to edit, update and destroy the image" do
+      subject.can?(:edit, @image).should be_true
+      subject.can?(:update, @image).should be_true
+      subject.can?(:destroy, @image).should be_true
+    end
+    it "should not be able to see the admin view of the image" do
+      subject.can?(:admin, @image).should be_false
+    end
+  end
+  context "a Repository Admin" do
+    before do
+      @user = FactoryGirl.find_or_create(:alice_admin)
+      Group.any_instance.stub :persist_to_ldap
+    end
+    subject { Ability.new(@user) }
+    it "should be able to discover the image" do
+      subject.can?(:discover, @image).should be_true
+    end
+    it "should be able to view the image" do
+      subject.can?(:read, @image).should be_true
+    end
+    it "should be able to edit, update and destroy the image" do
+      subject.can?(:edit, @image).should be_true
+      subject.can?(:update, @image).should be_true
+      subject.can?(:destroy, @image).should be_true
+    end
+    it "should not be able to see the admin view of the image" do
+      subject.can?(:admin, @image).should be_true
+    end
+  end
+end
+
+describe "given a Faculty-created image which NU has read access to" do
+  before do
+    @image = Multiresimage.find("inu:dil-nu-read-access-image")
+  end
+  context "someone with NU id" do
+    before do
+      @user = FactoryGirl.find_or_create(:nu_id_holder)
+      Group.any_instance.stub :persist_to_ldap
+    end
+    subject { Ability.new(@user) }
+    it "should be able to discover the image" do
+      subject.can?(:discover, @image).should be_true
+    end
+    it "should be able to view the image" do
+      subject.can?(:read, @image).should be_true
+    end
+    it "should not be able to edit, update and destroy the image" do
+      subject.can?(:edit, @image).should be_false
+      subject.can?(:update, @image).should be_false
+      subject.can?(:destroy, @image).should be_false
+    end
+    it "should not be able to see the admin view of the image" do
+      subject.can?(:admin, @image).should be_false
+    end
+  end
+end
+
+describe "given a Faculty-created image with collaborator" do
+  before do
+    @image = Multiresimage.find("inu:dil-nu-read-access-image")
+  end
+  context "a collaborator with edit access" do
+    before do
+      @user = FactoryGirl.find_or_create(:calvin_collaborator)
+      Group.any_instance.stub :persist_to_ldap
+    end
+    subject { Ability.new(@user) }
+    it "should be able to discover the image" do
+      subject.can?(:discover, @image).should be_true
+    end
+    it "should be able to view the image" do
+      subject.can?(:read, @image).should be_true
+    end
+    it "should be able to edit, update and destroy the image" do
+      subject.can?(:edit, @image).should be_true
+      subject.can?(:update, @image).should be_true
+      subject.can?(:destroy, @image).should be_true
+    end
+    it "should not be able to see the admin view of the image" do
+      subject.can?(:admin, @image).should be_false
+    end
+  end
+end
+
+describe "given a Faculty-created object where dept can read & NU can discover" do
+  before do
+    @image = Multiresimage.find("inu:dil-dept-access-image")
+  end
+  context "someone with NU id" do
+    before do
+      @user = FactoryGirl.find_or_create(:nu_id_holder)
+      Group.any_instance.stub :persist_to_ldap
+    end
+    subject { Ability.new(@user) }
+    it "should be able to discover the image" do
+      subject.can?(:discover, @image).should be_true
+    end
+    it "should not be able to view the image" do
+      subject.can?(:read, @image).should be_false
+    end
+    it "should not be able to edit, update and destroy the image" do
+      subject.can?(:edit, @image).should be_false
+      subject.can?(:update, @image).should be_false
+      subject.can?(:destroy, @image).should be_false
+    end
+    it "should not be able to see the admin view of the image" do
+      subject.can?(:admin, @image).should be_false
+    end
+  end
+  context "someone whose department has read access" do
+    before do
+      @user = FactoryGirl.find_or_create(:martia_morocco)
+      Group.any_instance.stub :persist_to_ldap
+    end
+    subject { Ability.new(@user) }
+    it "should be able to discover the image" do
+      subject.can?(:discover, @image).should be_true
+    end
+    it "should be able to view the image" do
+      subject.can?(:read, @image).should be_true
+    end
+    it "should not be able to edit, update and destroy the image" do
+      subject.can?(:edit, @image).should be_false
+      subject.can?(:update, @image).should be_false
+      subject.can?(:destroy, @image).should be_false
+    end
+    it "should not be able to see the admin view of the image" do
+      subject.can?(:admin, @image).should be_false
+    end
+  end
+end
+
 describe "a user" do
   before do
     @user = FactoryGirl.create(:staff)
@@ -45,6 +213,7 @@ describe "a user" do
       end
     end
   end
+
   context "who is an owner of a group" do
     before do
         @group = FactoryGirl.build(:user_group, :owner=>@user)
@@ -64,3 +233,51 @@ describe "a user" do
 
 end
 
+describe "Policy-enforcement: accessing an object whose" do
+  context "policy grants edit access to a group I belong to" do
+    before do
+      # @image = Multiresimage.new()
+      # @image.policy=@policy
+      # @image.save
+    end
+    it "should be able to view the image" do
+      pending
+      subject.can?(:read, @image).should be_true
+    end
+    it "should be able to destroy the image" do
+      pending
+      subject.can?(:destroy, @image).should be_true
+    end
+    it "should be able to edit the image" do
+      pending
+      subject.can?(:edit, @image).should be_true
+    end
+    it "should be able to update the image" do
+      pending
+      subject.can?(:update, @image).should be_true
+    end
+  end
+  context "policy grants read access to a group I belong to" do
+    before do
+      # @image = Multiresimage.new()
+      # @image.policy=@policy
+      # @image.save
+    end
+    it "should be able to view the image" do
+      pending
+      subject.can?(:read, @image).should be_true
+    end
+    it "should be able to destroy the image" do
+      pending
+      subject.can?(:destroy, @image).should be_false
+    end
+    it "should be able to edit the image" do
+      pending
+      subject.can?(:edit, @image).should be_false
+    end
+    it "should be able to update the image" do
+      pending
+      subject.can?(:update, @image).should be_false
+    end
+  end
+end
