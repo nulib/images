175ff4bac58347772e10409d2ef32c22fc03ae84
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-06-26 09:54:17 -0500

    Added link to manage policies

--- spec/requests/multiresimages_spec.rb
+++ spec/requests/multiresimages_spec.rb
@@ -13,6 +13,7 @@ describe "View a multiresimage" do
       click_on("Edit")
       page.should have_selector("#sidebar a[href='#{catalog_index_path}']", :text=>'Search')
     end
+
   end
 
   describe "as a staff member" do
@@ -24,6 +25,10 @@ describe "View a multiresimage" do
       page.should have_link('EXIF Technical Metadata')
       page.should have_link('MIX Technical Metadata')
       page.should have_link('MIX Technical Metadata for JP2')
+    end
+    it "should edit the object" do
+      visit edit_multiresimage_path('inu:dil-d42f25cc-deb2-4fdc-b41b-616291578c26')
+      page.should have_link "manage policies"
     end
   end
 
