6b4d6f463f426539b12b2b5758c716fdf9401269
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-06-26 10:49:23 -0500

    Add access permissions for the creator when creating a policy

--- spec/controllers/policies_controller_spec.rb
+++ spec/controllers/policies_controller_spec.rb
@@ -35,80 +35,77 @@ describe PoliciesController do
     end
   end
 
-  describe "new" do
+  describe "as the archivist" do
+
     before do
       @policy = AdminPolicy.create
       @user = FactoryGirl.find_or_create(:archivist)
       Hydra::LDAP.should_receive(:groups_for_user).with(@user.uid).and_return([])
       sign_in @user
     end
-    it "should draw the form" do
+    describe "new" do
+      it "should draw the form" do
         get :new
         assigns["policy"].should be_kind_of AdminPolicy
-        assigns["policy"].edit_users.should == 'archivist'
         response.should be_successful
-    end
-  end
-
-  describe "create" do
-    it "should save the new policy and creator should have edit perms" do
-      post :create, :admin_policy=>{:title=>'My title'}
-      response.should redirect_to policies_path
-      assigns['policy'].title.should == 'My title'
-    end
-  end
-
-  describe "update" do
-    before do
-      @policy = AdminPolicy.create
-      @user = FactoryGirl.find_or_create(:archivist)
-      Hydra::LDAP.should_receive(:groups_for_user).with(@user.uid).and_return([])
-      sign_in @user
-    end
-    describe "that I have edit permissions on" do
-      before do
-        @policy.apply_depositor_metadata(@user.uid)
-        @policy.edit_groups = ["staff"]
-        @policy.save
       end
-      it "should save changes to the policy" do
-        put :update, :id=>@policy.pid, :admin_policy=>{:title =>"New title"}
-        response.should redirect_to(policies_path())
-        assigns[:policy].title.should == "New title"
-        flash[:notice].should == "Saved changes to New title"
+    end
+
+    describe "create" do
+      it "should save the new policy and creator should have edit perms" do
+        post :create, :admin_policy=>{:title=>'My title'}
+        response.should redirect_to policies_path
+        assigns['policy'].title.should == 'My title'
+        assigns["policy"].edit_users.should == ['archivist1']
       end
-      
-      describe "setting permissions" do
-        it "should update permissions" do
-          put :update, :id=>@policy.pid, :admin_policy=>{"permissions"=>{"group"=>{"staff"=>"edit", "faculty"=>"edit"}, "user"=>{"student1"=>"discover","vanessa"=>"edit", "archivist1"=>"read"}, 
-          "new_group_name"=>"", "new_group_permission"=>"none", 
-          "new_user_name"=>"", "new_user_permission"=>"none"},
-          "default_permissions"=>{"group"=>{"staff"=>"edit", "faculty"=>"edit"}, "user"=>{"student1"=>"discover","vanessa"=>"edit", "archivist1"=>"read"}, 
-          "new_group_name"=>"", "new_group_permission"=>"none", 
-          "new_user_name"=>"", "new_user_permission"=>"none"}}
-          updated_policy = AdminPolicy.find(@policy.pid)
-          updated_policy.edit_groups.should include("staff")
-          updated_policy.edit_groups.should include("faculty") 
-          updated_policy.discover_users.should include("student1")
-          updated_policy.edit_users.should include("vanessa")
-          updated_policy.read_users.should include("archivist1")
-          updated_policy.defaultRights.individuals.should == {"student1"=>"discover","vanessa"=>"edit", "archivist1"=>"read"}
+    end
+
+    describe "update" do
+      describe "that I have edit permissions on" do
+        before do
+          @policy.apply_depositor_metadata(@user.uid)
+          @policy.edit_groups = ["staff"]
+          @policy.save
         end
-        it "should add group & user permissions without wiping out existing permissions" do
-          # pre-existing permissions
-          @policy.edit_groups.should include("staff") 
-          @policy.edit_users.should include(@user.uid)
-          
-          put :update, :id=>@policy.pid, :admin_policy=>{"permissions"=>{
-            "new_group_name"=>"mynewgroup", "new_group_permission"=>"discover", 
-            "new_user_name"=>"uuuusssserzed", "new_user_permission"=>"edit"}}
-          updated_policy = AdminPolicy.find(@policy.pid)
-          # check that new permissions were granted
-          updated_policy.discover_groups.should include("mynewgroup")
-          updated_policy.edit_users.should include("uuuusssserzed")
-          # check that the original permissions weren't changed
-          updated_policy.edit_groups.should include("staff") 
-          updated_policy.edit_users.should include(@user.uid)
+        it "should save changes to the policy" do
+          put :update, :id=>@policy.pid, :admin_policy=>{:title =>"New title"}
+          response.should redirect_to(policies_path())
+          assigns[:policy].title.should == "New title"
+          flash[:notice].should == "Saved changes to New title"
+        end
+        
+        describe "setting permissions" do
+          it "should update permissions" do
+            put :update, :id=>@policy.pid, :admin_policy=>{"permissions"=>{"group"=>{"staff"=>"edit", "faculty"=>"edit"}, "user"=>{"student1"=>"discover","vanessa"=>"edit", "archivist1"=>"read"}, 
+            "new_group_name"=>"", "new_group_permission"=>"none", 
+            "new_user_name"=>"", "new_user_permission"=>"none"},
+            "default_permissions"=>{"group"=>{"staff"=>"edit", "faculty"=>"edit"}, "user"=>{"student1"=>"discover","vanessa"=>"edit", "archivist1"=>"read"}, 
+            "new_group_name"=>"", "new_group_permission"=>"none", 
+            "new_user_name"=>"", "new_user_permission"=>"none"}}
+            updated_policy = AdminPolicy.find(@policy.pid)
+            updated_policy.edit_groups.should include("staff")
+            updated_policy.edit_groups.should include("faculty") 
+            updated_policy.discover_users.should include("student1")
+            updated_policy.edit_users.should include("vanessa")
+            updated_policy.read_users.should include("archivist1")
+            updated_policy.defaultRights.individuals.should == {"student1"=>"discover","vanessa"=>"edit", "archivist1"=>"read"}
+          end
+          it "should add group & user permissions without wiping out existing permissions" do
+            # pre-existing permissions
+            @policy.edit_groups.should include("staff") 
+            @policy.edit_users.should include(@user.uid)
+            
+            put :update, :id=>@policy.pid, :admin_policy=>{"permissions"=>{
+              "new_group_name"=>"mynewgroup", "new_group_permission"=>"discover", 
+              "new_user_name"=>"uuuusssserzed", "new_user_permission"=>"edit"}}
+            updated_policy = AdminPolicy.find(@policy.pid)
+            # check that new permissions were granted
+            updated_policy.discover_groups.should include("mynewgroup")
+            updated_policy.edit_users.should include("uuuusssserzed")
+            # check that the original permissions weren't changed
+            updated_policy.edit_groups.should include("staff") 
+            updated_policy.edit_users.should include(@user.uid)
+          end
         end
       end
     end
