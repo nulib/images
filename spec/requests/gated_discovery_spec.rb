6e2428d43442a81e6532b15bc32d2381a3174671
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2012-06-26 16:42:02 -0500

    Gated discovery test coverage (except policy permissions and admin access, which are not implemented yet.)

--- spec/requests/gated_discovery_spec.rb
+++ spec/requests/gated_discovery_spec.rb
@@ -1,83 +1,69 @@
 require 'spec_helper'
 
 describe "When I am searching for content" do
-  context "Given a Faculty-created image with no custom access set" do
-    before do
-      @image = Multiresimage.find("inu:dil-default-access-image")
-    end
+  context "Given an image with no custom access set" do
+    before { @pid = "inu:dil-default-access-image" }
     context "Then someone with NU id" do
-      before do
-        @user = FactoryGirl.find_or_create(:nu_id_holder)
-        Group.any_instance.stub :persist_to_ldap
+      before { login FactoryGirl.find_or_create(:nu_id_holder) }
+      before { visit catalog_index_path(:q=>@pid) }
+      it "should not be able to discover the image" do
+        page.should_not have_selector("a[href='#{multiresimage_path(@pid)}']")
       end
-      subject { Ability.new(@user) }
-      it "should not be able to discover the image" 
     end
     context "Then the Creator" do
-      before do
-        @user = FactoryGirl.find_or_create(:joe_creator)
-        Group.any_instance.stub :persist_to_ldap
-      end
-      subject { Ability.new(@user) }
-      it "should be able to discover the image" 
+      before { login FactoryGirl.find_or_create(:joe_creator) }
+      before { visit catalog_index_path(:q=>"Tibetan_astronomical_Thangka_4") }
+      it "should be able to discover the image" do
+        page.should have_selector("a[href='#{multiresimage_path(@pid)}']")
+      end 
     end
     context "Then a Repository Admin" do
-      before do
-        @user = FactoryGirl.find_or_create(:alice_admin)
-        Group.any_instance.stub :persist_to_ldap
+      before { login FactoryGirl.find_or_create(:alice_admin) }
+      before { visit multiresimage_path(@pid) }
+      it "should be able to discover the image" do
+        pending "Repo admin permissions"
+        page.should have_selector("a[href='#{multiresimage_path(@pid)}']")
       end
-      subject { Ability.new(@user) }
-      it "should be able to discover the image" 
     end
   end
 
-  context "Given a Faculty-created image which NU has read access to" do
-    before do
-      @image = Multiresimage.find("inu:dil-nu-read-access-image")
-    end
+  context "Given an image which NU has read access to" do
+    before { @pid = "inu:dil-nu-read-access-image" }
     context "The someone with NU id" do
-      before do
-        @user = FactoryGirl.find_or_create(:nu_id_holder)
-        Group.any_instance.stub :persist_to_ldap
+      before { login FactoryGirl.find_or_create(:nu_id_holder) }
+      before { visit catalog_index_path(:q=>@pid) }
+      it "should be able to discover the image" do
+        page.should have_selector("a[href='#{multiresimage_path(@pid)}']", :text=> "The Fabric of the Human Body (1992). Muscles an...")
       end
-      subject { Ability.new(@user) }
-      it "should be able to discover the image" 
     end
   end
 
-  context "Given a Faculty-created image with collaborator" do
-    before do
-      @image = Multiresimage.find("inu:dil-nu-read-access-image")
-    end
+  context "Given an image with collaborator" do
+    before { @pid = "inu:dil-nu-read-access-image" }
     context "Then a collaborator with edit access" do
-      before do
-        @user = FactoryGirl.find_or_create(:calvin_collaborator)
-        Group.any_instance.stub :persist_to_ldap
-      end
-      subject { Ability.new(@user) }
-      it "should be able to discover the image" 
+      before { login FactoryGirl.find_or_create(:calvin_collaborator) }
+      before { visit catalog_index_path(:q=>@pid) }
+      it "should be able to discover the image" do
+        page.should have_selector("a[href='#{multiresimage_path(@pid)}']", :text=> "The Fabric of the Human Body (1992). Muscles an...")
+      end 
     end
   end
   
-  context "Given a Faculty-created object where dept can read & NU can discover" do
-    before do
-      @image = Multiresimage.find("inu:dil-dept-access-image")
-    end
+  context "Given an image where dept can read & NU can discover" do
+    before { @pid = "inu:dil-dept-access-image" }
     context "Then someone with NU id" do
-      before do
-        @user = FactoryGirl.find_or_create(:nu_id_holder)
-        Group.any_instance.stub :persist_to_ldap
-      end
-      subject { Ability.new(@user) }
-      it "should be able to discover the image" 
+      before { login FactoryGirl.find_or_create(:nu_id_holder) }
+      before { visit catalog_index_path(:q=>@pid) }
+      it "should be able to discover the image" do
+        page.should have_selector("a[href='#{multiresimage_path(@pid)}']")
+      end 
     end
     context "Then someone whose department has read access" do
-      before do
-        @user = FactoryGirl.find_or_create(:martia_morocco)
-        Group.any_instance.stub :persist_to_ldap
+      before { login FactoryGirl.find_or_create(:martia_morocco) }
+      before { visit catalog_index_path(:q=>@pid) }
+      it "should be able to discover the image" do
+        page.should have_selector("a[href='#{multiresimage_path(@pid)}']")
       end
-      subject { Ability.new(@user) }
-      it "should be able to discover the image" 
     end
   end
 
@@ -89,7 +75,7 @@ describe "When I am searching for content" do
       # @image.save
     end
     it "Then I should be able to discover the image" do
-      pending
+      pending "Policy permissions"
       subject.can?(:discover, @image).should be_true
     end
   end
@@ -100,7 +86,7 @@ describe "When I am searching for content" do
       # @image.save
     end
     it "Then I should be able to discover the image" do
-      pending
+      pending "Policy permissions"
       subject.can?(:discover, @image).should be_true
     end
   end
