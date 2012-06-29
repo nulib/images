7c183a9db9eb62e1d139d99434734f84501bebc6
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-06-29 12:41:59 -0500

    DIL-539 Batch modify policy association

--- spec/requests/batch_update_spec.rb
+++ spec/requests/batch_update_spec.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 require "spec_helper"
 
 describe "Select images to update" do
@@ -7,16 +9,24 @@ describe "Select images to update" do
       login FactoryGirl.find_or_create(:archivist)
     end
     it "should have a form" do
-      pending
       fill_in("q", :with=>"Phillip")
       click_on("Submit")
 
-      page.should have_selector("input[type='checkbox'][id='all']")
-      page.should have_selector("input[type='submit'][class='folder_submit'][id='folder_submit_inu:dil-d42f25cc-deb2-4fdc-b41b-616291578c26']")
+      page.should have_selector("input.batch-all-button[type='submit'][value='Use all results']")
+
+      page.should have_selector("input[type='submit'][class='batch_submit'][id='batch_submit_inu:dil-d42f25cc-deb2-4fdc-b41b-616291578c26']")
       # The blacklight javascript transforms the above element into this:
       #  input[type='checkbox'][class='toggle_folder'][id='toggle_folder_inu:dil-d42f25cc-deb2-4fdc-b41b-616291578c26']
 
-      page.should have_selector("form.button_to input[type=submit][value='Edit Batch']")
+      click_on "Use all results"
+      page.should have_selector("form[action='#{batch_edits_path}']")
+
+      select "Default Policy — Private to Owner", :from=>"Policy"
+
+      click_on "Save changes"
+      
+      page.should have_content("Batch update complete")
+
       
     end
   end
