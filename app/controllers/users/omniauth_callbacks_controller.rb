3bd17a503b5d7d289da3217f8edab8f005bf3c0f
Mike Stroming <mstrom81@gmail.com>
2012-09-13 11:17:42 -0500

    Added exception handling for ldap

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,15 +2,19 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def ldap
     @user = User.find_for_ldap_oauth(request.env["omniauth.auth"], current_user)
 
-    # eduPersonAffiliation (e.g. "staff, student, faculty")
-    @user.affiliations = request.env["omniauth.auth"][:extra][:raw_info][:edupersonaffiliation]
-
-    if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Ldap"
-      sign_in_and_redirect @user, :event => :authentication
-    else
-      session["devise.ldap_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
+   begin
+     # eduPersonAffiliation (e.g. "staff, student, faculty")
+     @user.affiliations = request.env["omniauth.auth"][:extra][:raw_info][:edupersonaffiliation]
+   rescue Exception=>e
+     logger.error("LDAP Error:" << e.message)
+   ensure
+      if @user.persisted?
+        flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Ldap"
+        sign_in_and_redirect @user, :event => :authentication
+      else
+        session["devise.ldap_data"] = request.env["omniauth.auth"]
+        redirect_to new_user_registration_url
+      end
+   end
   end
 end
