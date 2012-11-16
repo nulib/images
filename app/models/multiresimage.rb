65f04d1859d6436d161c465936091da5900126e8
Mike Stroming <mstrom81@gmail.com>
2012-11-16 15:36:13 -0600

    Fix image vra xml not getting updated correctly after upload

--- app/models/multiresimage.rb
+++ app/models/multiresimage.rb
@@ -121,10 +121,9 @@ class Multiresimage < ActiveFedora::Base
 
   #update the VRA ref id value
   def update_ref_id(ref_id)
-    #node_set = self.datastreams["VRA"].ng_xml.xpath('/vra:vra/vra:image[@refid]')
-    node_set = self.datastreams["VRA"].find_by_terms('/vra:vra/vra:image[@refid]')
+    node_set = self.datastreams["VRA"].ng_xml.xpath('/vra:vra/vra:image[@refid]')
     node_set[0].set_attribute("refid", ref_id)
-    #self.save!
+    self.datastreams["VRA"].dirty = true
   end
   
   #replace the VRA locationSet_display value
@@ -136,6 +135,7 @@ class Multiresimage < ActiveFedora::Base
   def replace_locationset_location_pid(new_pid)
     node_set = self.datastreams["VRA"].find_by_terms("/vra:vra/vra:image/vra:locationSet/vra:location/vra:refid[@source='DIL']")
     node_set[0].content = new_pid
+    self.datastreams["VRA"].dirty = true
   end
   
   #replace every instance of old pid with new pid in VRA
@@ -155,7 +155,7 @@ class Multiresimage < ActiveFedora::Base
     node_set[0].set_attribute("relids", work_pid)
     node_set[0].set_attribute("type", "imageOf")
     node_set[0].set_attribute("label", "Image")
-    #self.save!
+    self.datastreams["VRA"].dirty = true
   end
   
   def get_work_pid
