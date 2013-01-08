1102e8fa0d170cba248594cc6fb36d3cf7eddb06
Mike Stroming <mstrom81@gmail.com>
2013-01-08 16:35:01 -0600

    Updated initializers for Rails security issue

--- config/initializers/load_dil_configs.rb
+++ config/initializers/load_dil_configs.rb
@@ -1,1 +1,3 @@
 DIL_CONFIG = YAML.load_file(Rails.root.join('config', 'dil-config.yml'))[Rails.env]
+ActiveSupport::XmlMini::PARSING.delete("symbol")
+ActiveSupport::XmlMini::PARSING.delete("yaml") 
