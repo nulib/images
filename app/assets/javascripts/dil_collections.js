1a30c52aee06ac87993e1c43f42078f0365f3cbb
egspoony <edgar-garcia@northwestern.edu>
2013-01-17 14:53:12 -0600

    Fixed bugs with the javascript.

--- app/assets/javascripts/dil_collections.js
+++ app/assets/javascripts/dil_collections.js
@@ -1,62 +1,63 @@
+function dropMe(theObj) {
+	$(theObj).droppable({
+	activeClass: "ui-state-default",
+	hoverClass: "ui-state-hover",
+	accept: ":not(.ui-sortable-helper)",
+	drop: function( event, ui ) {
+		$( this ).find( ".placeholder" ).remove();
+		
+		//get id attribute for draggable <li> item (image)
+		var imageID = $(ui.draggable).attr("pid");
+		
+		//get title attribute for draggable <li> item (image)
+		var titleID = $(ui.draggable).attr("title");
+		
+		//get member_type attribute for draggable <li> item (image)
+		//var memberType = $(ui.draggable).attr("member_type");
+
+		//get id attribute for droppable <li> item (collection)
+		//var collectionID = $( this ).find("li").attr("pid");
+		var collectionID = $( this ).attr("pid");
+		
+		
+		
+		$( "<li></li>" ).text(ui.draggable.attr("title")+" added!").appendTo(this);
+		
+		$.ajax({
+		type: "POST",
+		url: "dil_collections/add/" + collectionID + "/" + imageID + "?member_title=" + titleID,
+		//data: "id=10",
+		async: false,
+		success: function(msg){
+		}
+		});//end ajax
+		
+	}//end droppable
+}).sortable({
+	items: "li:not(.placeholder)",
+	sort: function() {
+		// gets added unintentionally by droppable interacting with sortable
+		// using connectWithSortable fixes this, but doesn't allow you to customize active/hoverClass options
+		$( this ).removeClass( "ui-state-default" );
+	}
+})};
 
 	start_index='';
 	$(document).ready(function(){
 		// For adding items to the collection
 		//For sidebar drag/drop
-		$( "#images li" ).live('mouseover', function() {
+		$(document).on('mouseover', "#images li", function() {
 			$(this).draggable({
 			appendTo: "body",
 			helper: "clone"
 		})});
-		$( ".accordion h2" ).live('mouseover', function() {
+		$(document).on('mouseover', ".accordion h2", function() {
 			$(this).draggable({
 			appendTo: "body",
 			helper: "clone"
 		})});
-		$( ".accordion h2" ).live('mousedown', function() {
-			$(this).droppable({
-			activeClass: "ui-state-default",
-			hoverClass: "ui-state-hover",
-			accept: ":not(.ui-sortable-helper)",
-			drop: function( event, ui ) {
-				$( this ).find( ".placeholder" ).remove();
-				
-				//get id attribute for draggable <li> item (image)
-				var imageID = $(ui.draggable).attr("pid");
-				
-				//get title attribute for draggable <li> item (image)
-				var titleID = $(ui.draggable).attr("title");
-				
-				//get member_type attribute for draggable <li> item (image)
-				//var memberType = $(ui.draggable).attr("member_type");
 
-				//get id attribute for droppable <li> item (collection)
-				//var collectionID = $( this ).find("li").attr("pid");
-				var collectionID = $( this ).attr("pid");
-				
-				
-				
-				$( "<li></li>" ).text(ui.draggable.attr("title")+" added!").appendTo(this);
-				
-				$.ajax({
-				type: "POST",
-				url: "dil_collections/add/" + collectionID + "/" + imageID + "?member_title=" + titleID,
-				//data: "id=10",
-				async: false,
-				success: function(msg){
-				}
-				});//end ajax
-				
-			}//end droppable
-		})}).sortable({
-			items: "li:not(.placeholder)",
-			sort: function() {
-				// gets added unintentionally by droppable interacting with sortable
-				// using connectWithSortable fixes this, but doesn't allow you to customize active/hoverClass options
-				$( this ).removeClass( "ui-state-default" );
-			}
-		});
-
+		dropMe(".accordion h2");
 		//End for sidebar drag/drop
 
 		//For moving items around in the collection
@@ -130,6 +131,7 @@
 			    'class': 'accordion ui-widget-content',
 			    html: items.join('')
 			  }).appendTo(theObj.siblings('div').children('div.inner'));
+			theObj.siblings('div').children('div.inner').find('h2').each(function(index) { dropMe(this) });
 			});//End Ajax call
 
 		} else {
