79d9b60796ed12d4015b0a42690da7245bf04f34
egspoony <edgar-garcia@northwestern.edu>
2013-03-26 11:43:50 -0500

    Added better exception handling messages by creating an ExceptionsController, also added the layout.

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ end
 
 module DIL
   class Application < Rails::Application
+    config.exceptions_app = ->(env) { ExceptionsController.action(:show).call(env) }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
