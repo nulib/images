5087c824479eb3e0ef9208a96d7a3b3ac2c772d9
Mike Stroming <mstrom81@gmail.com>
2013-05-21 17:25:50 -0500

    Batch script to update inline datastreams to managed datastreams

---
+++ update_objects_control_group.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/evn ruby
+
+Multiresimage.find_each {|image|
+
+  datastreams = ["ARCHV_TECHMD", "ARCHV_EXIF", "DELIV_TECHMD"]
+
+  datastreams.each {|ds_name|
+    if !image.send(ds_name).nil? and image.send(ds_name).controlGroup != "M"
+ 
+      begin
+        #get the xml
+        techmd_xml = image.send(ds_name).content
+  
+        #delete the ds
+        image.send(ds_name).delete
+  
+        #save the image
+        image.save
+  
+        #reload the image
+        image = Multiresimage.find(image.pid)
+        
+        image.send(ds_name).content = techmd_xml 
+        
+        image.send(ds_name).controlGroup="M"
+        
+        image.save
+        
+        puts "Updated #{ds_name} for #{image.pid}"
+        
+      rescue Exception => e
+       puts e.message
+      end
+   else
+    puts "No update to #{ds_name} for #{image.pid}"
+  end
+  }
+}
+
