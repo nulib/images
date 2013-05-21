ee397ba3fb58f5382d1e0796638c8b67936595a6
Mike Stroming <mstrom81@gmail.com>
2013-05-21 17:35:22 -0500

    Added comments to script

--- update_objects_control_group.rb
+++ update_objects_control_group.rb
@@ -1,14 +1,23 @@
 #!/usr/bin/evn ruby
 
-datastreams = ["ARCHV_TECHMD", "ARCHV_EXIF", "DELIV_TECHMD"]
-  
-Multiresimage.find_each {|image|
+# The controlGroup for a datastream cannot be changed after the datastream is created.
+# In order to change the contolGroup, you need to copy the content of the ds, delete the ds, then
+# re-create the ds with the old content and the new controlGroup.  Note: This deletes all audit trail
+# for the datastream.  We are running this locally and in staging.
 
+# List of datastreams to change controlGroup to M
+datastreams = ["ARCHV_TECHMD", "ARCHV_EXIF", "DELIV_TECHMD"]
+
+# For each image object 
+Multiresimage.find_each {|image|
+  
+  # For each datastream
   datastreams.each {|ds_name|
+    # If the object has the datastream with a controlGroup that isn't "M"
     if !image.send(ds_name).nil? and image.send(ds_name).controlGroup != "M"
  
       begin
-        #get the xml
+        #get the xml from the ds
         techmd_xml = image.send(ds_name).content
   
         #delete the ds
@@ -20,10 +29,13 @@ Multiresimage.find_each {|image|
         #reload the image
         image = Multiresimage.find(image.pid)
         
+        #set the content of the ds to the previous content
         image.send(ds_name).content = techmd_xml 
         
+        #set the controlGroup
         image.send(ds_name).controlGroup="M"
         
+        #save the ds
         image.save
         
         puts "Updated #{ds_name} for #{image.pid}"
