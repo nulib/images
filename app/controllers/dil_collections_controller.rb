b50c62178d31678733d898903aee7c8a7d071186
Paul Clough <paclough@github>
2013-11-21 11:14:26 -0600

    DIL-2333: Adding comments to the modified files

--- app/controllers/dil_collections_controller.rb
+++ app/controllers/dil_collections_controller.rb
@@ -255,25 +255,28 @@ end
   
   
   
-  #This will return all the subcollections of the collection
+  # This will return a JSON string for all the subcollections of the collection
   def get_subcollections
     begin 
       collection = DILCollection.find(params[:id])
       authorize! :show, collection
     
-      #get the json
+      # Get the subcollection JSON as a string and convert to a hash
       return_json = JSON.parse( collection.get_subcollections_json )
-      logger.debug( "return_json: #{return_json}" )
+      # For each subcollection ...
       return_json.each do |subcoll|
+        # Create a DILCollection object
         coll = DILCollection.find( subcoll[ "pid" ] )
-        logger.debug( "collection_owner: #{coll.owner}")
+        # Check to see if the current_user is an admin
         if current_user.admin?
+          # If so, add the owner to the JSON
+          # If the owner is empty, the JSON will contain "null"
           subcoll[ "owner" ] = coll.owner
         end
-        logger.debug( "collection: #{collection}")
       end
-      return_json.to_json
       logger.debug( "return_json: #{ return_json }" )
+      # Return the JSON as a string
+      return_json.to_json
 
     rescue Exception => e
       #error
