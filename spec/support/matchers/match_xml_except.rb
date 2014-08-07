6bbc77c5fc8c3f0113ed048c573a3511af6186aa
Paul Clough <p-clough@northwestern.edu>
2014-08-06 22:25:05 -0500

    Update custom matcher to be positive. Check for an actual tag.

--- spec/support/matchers/match_xml_except.rb
+++ spec/support/matchers/match_xml_except.rb
@@ -3,15 +3,15 @@ RSpec::Matchers.define :match_xml_except do |*elements|
   xml_new = elements.shift
   element = elements.first
   match do |xml_orig|
-    line_diff = false
+    line_eql = true
     xml_new_array = xml_new.lines.to_a
     xml_orig_array = xml_orig.lines.to_a
     xml_orig_array.each_with_index do |line, count|
-      break if line_diff
-      next if element && line.include?( element )
-      line_diff = line != xml_new_array[ count ]
+      break unless line_eql
+      next if element && line.include?( "<#{ element}>" )
+      line_eql = line == xml_new_array[ count ]
     end
-    !line_diff
+    line_eql
   end
 
     # Optional failure messages
