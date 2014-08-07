b5b50b8ba9c9b956fc2c744ecb92c3b96f1ea20b
Paul Clough <p-clough@northwestern.edu>
2014-08-07 11:40:35 -0500

    Fix check for tag in matcher

--- spec/support/matchers/match_xml_except.rb
+++ spec/support/matchers/match_xml_except.rb
@@ -3,15 +3,15 @@ RSpec::Matchers.define :match_xml_except do |*elements|
   xml_new = elements.shift
   element = elements.first
   match do |xml_orig|
-    line_eql = true
+    lines_eql = true
     xml_new_array = xml_new.lines.to_a
     xml_orig_array = xml_orig.lines.to_a
     xml_orig_array.each_with_index do |line, count|
-      break unless line_eql
-      next if element && line.include?( "<#{ element}>" )
-      line_eql = line == xml_new_array[ count ]
+      break unless lines_eql
+      next if element && line.include?( "<#{ element}" )
+      lines_eql = line == xml_new_array[ count ]
     end
-    line_eql
+    lines_eql
   end
 
     # Optional failure messages
@@ -28,3 +28,4 @@ RSpec::Matchers.define :match_xml_except do |*elements|
     "checks to see if two XML strings match, except for the supplied element"
   end
 end
+
