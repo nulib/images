280a9047ebc28eda9a3d7b133e7dfbe3ddd238a7
Chris Syversen <csyversen@gmail.com>
2014-08-28 13:02:48 -0500

    load the XSD once in initializer

--- app/helpers/multiresimage_helper.rb
+++ app/helpers/multiresimage_helper.rb
@@ -1,16 +1,11 @@
 module MultiresimageHelper
 
-  require 'open-uri'
-
-  VRA_SCHEMA = File.read("#{Rails.root}/app/assets/xml/vra-strict.xsd")
-
   # returns nil is there weren't any validation errors
   def self.validate_vra( vra )
     doc = Nokogiri::XML( vra )
-    xsd = Nokogiri::XML::Schema(VRA_SCHEMA)
 
     invalid = ""
-    xsd.validate(doc).each do |error|
+    XSD.validate(doc).each do |error|
       invalid << "Validation error: #{error.message}\n"
     end
 
@@ -23,10 +18,9 @@ module MultiresimageHelper
 
 
   def self.valid_vra?( vra )
-    xsd = Nokogiri::XML::Schema(VRA_SCHEMA)
     doc = Nokogiri::XML( vra )
 
-    xsd.valid?(doc)
+    XSD.valid?(doc)
   end
 
 end
