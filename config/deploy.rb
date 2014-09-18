537268e29d24a5c2d19f3570b435092b41e8a0f3
Chris Syversen <csyversen@gmail.com>
2014-09-18 16:12:16 -0500

    changing log location

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,7 +42,7 @@ namespace :deploy do
       # Your restart mechanism here, for example:
       #execute :touch, release_path.join('tmp/restart.txt')
       `kill $(ps -aef | grep '[u]nicorn_rails master' | awk '{print $2}')`
-      `nohup bundle exec unicorn_rails -p 3000 -E staging > /var/www/dil_hydra/unicorn.log 2>&1 &`
+      `nohup bundle exec unicorn_rails -p 3000 -E staging > unicorn.log 2>&1 &`
     end
   end
 
