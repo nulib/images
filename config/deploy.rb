9bc7baad1a93c83e2a09f769c90e8f8aeb85e6dc
Paul Clough <p-clough@northwestern.edu>
2014-09-19 10:29:36 -0500

    Two executions

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,7 +41,8 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       #execute :touch, release_path.join('tmp/restart.txt')
-      execute "kill $(ps -aef | grep '[u]nicorn_rails master' | awk '{print $2}'); nohup bundle exec unicorn_rails -p 3000 -E staging > /var/www/dil_hydra/unicorn.log 2>&1 &"
+      execute "kill $(ps -aef | grep '[u]nicorn_rails master' | awk '{print $2}')"
+      execute "nohup bundle exec unicorn_rails -p 3000 -E staging > /var/www/dil_hydra/unicorn.log 2>&1 &"
     end
   end
 
