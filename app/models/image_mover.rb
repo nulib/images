3d49b5783be31f892a8c94415716f72b6a3f6b2f
Paul Clough <p-clough@northwestern.edu>
2014-09-24 15:39:30 -0500

    Fix typo

--- app/models/image_mover.rb
+++ app/models/image_mover.rb
@@ -17,7 +17,7 @@ class ImageMover < ActiveRecord::Base
       logger.debug DIL_CONFIG[ 'jp2_server' ]
       logger.debug DIL_CONFIG[ 'jp2_ssh_user' ]
       logger.debug DIL_CONFIG[ 'jp2_ssh_pw' ]
-      scp_mover( server: DIL_CONIFG['jp2_server'], user: DIL_CONFIG[ 'jp2_ssh_user' ], password: DIL_CONFIG[ 'jp2_ssh_pw' ], local_img_path: jp2_img_path, remote_img_path: repo_location )
+      scp_mover( server: DIL_CONFIG['jp2_server'], user: DIL_CONFIG[ 'jp2_ssh_user' ], password: DIL_CONFIG[ 'jp2_ssh_pw' ], local_img_path: jp2_img_path, remote_img_path: repo_location )
     end
   end
 
@@ -36,7 +36,7 @@ class ImageMover < ActiveRecord::Base
       logger.debug DIL_CONFIG[ 'repo_server' ]
       logger.debug DIL_CONFIG[ 'repo_ssh_user' ]
       logger.debug DIL_CONFIG[ 'repo_ssh_pw' ]
-      scp_mover( server: DIL_CONIFG['repo_server'], user: DIL_CONFIG[ 'repo_ssh_user' ], password: DIL_CONFIG[ 'repo_ssh_pw' ], local_img_path: tiff_img_path, remote_img_path: repo_location )
+      scp_mover( server: DIL_CONFIG['repo_server'], user: DIL_CONFIG[ 'repo_ssh_user' ], password: DIL_CONFIG[ 'repo_ssh_pw' ], local_img_path: tiff_img_path, remote_img_path: repo_location )
     end
   end
 
