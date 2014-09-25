3bb33297930e399b6bfb821c11e0999893ee1e79
Paul Clough <p-clough@northwestern.edu>
2014-09-25 09:58:26 -0500

    Don't log passwords, duh

--- app/models/image_mover.rb
+++ app/models/image_mover.rb
@@ -16,7 +16,6 @@ class ImageMover < ActiveRecord::Base
 
       logger.debug DIL_CONFIG[ 'jp2_server' ]
       logger.debug DIL_CONFIG[ 'jp2_ssh_user' ]
-      logger.debug DIL_CONFIG[ 'jp2_ssh_pw' ]
       scp_mover( server: DIL_CONFIG['jp2_server'], user: DIL_CONFIG[ 'jp2_ssh_user' ], password: DIL_CONFIG[ 'jp2_ssh_pw' ], local_img_path: jp2_img_path, remote_img_path: repo_location )
     end
   end
@@ -35,7 +34,6 @@ class ImageMover < ActiveRecord::Base
 
       logger.debug DIL_CONFIG[ 'repo_server' ]
       logger.debug DIL_CONFIG[ 'repo_ssh_user' ]
-      logger.debug DIL_CONFIG[ 'repo_ssh_pw' ]
       scp_mover( server: DIL_CONFIG['repo_server'], user: DIL_CONFIG[ 'repo_ssh_user' ], password: DIL_CONFIG[ 'repo_ssh_pw' ], local_img_path: tiff_img_path, remote_img_path: repo_location )
     end
   end
@@ -48,7 +46,6 @@ class ImageMover < ActiveRecord::Base
     require 'net/scp'
 
     logger.debug "UPLOADING ..."
-    logger.debug options
     Net::SCP.upload!( options[:server],
                       options[:user],
                       options[:local_img_path],
