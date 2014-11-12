710c2007b53629ebdb445da6d9aa3c11679802aa
Chris Syversen <csyversen@gmail.com>
2014-11-12 09:43:15 -0600

    adding a note saying this script needs the ruby-xslt gem

--- lib/import_collection.rb
+++ lib/import_collection.rb
@@ -15,7 +15,7 @@
 # with it's pid after the collection is created and if it has subcollections.  Thay way, a subcollection can look at it's parent folder and know
 # it's pid.  That's how this code supports as many levels of nesting as needed.
 #
-# Notes: 
+# Notes:
 #   Parent collections can have images (or not), along with subcollections.
 #   Subcollections can also be parent collections.
 #
@@ -26,7 +26,7 @@
 #
 # collection
 #	xml
-#	
+#
 # collection
 #	subcollection
 #		xml
@@ -35,6 +35,8 @@
 #	subcollection
 #		subcollection
 #			xml
+
+# this script requires the ruby-xslt gem
 
 require 'xml/xslt'
 require 'fileutils'
@@ -45,63 +47,63 @@ include DIL::PidMinter
 def read_folder(path)
 
   logger.debug("read folder #{path}")
-  
+
   #if root folder of import, skip to next folder since we don't want to create a collection for that folder
   if (File.expand_path("..",path) == '/usr/local/src/dil_hydra')
     #get a listing for the root folder
-    Dir.glob("#{path}/*") do |name|   
+    Dir.glob("#{path}/*") do |name|
       #if item is a directory (collection), make recursive call
       if FileTest.directory? name
         #logger.debug("#{name} is directory")
         read_folder(name)
       end
     end
-  
+
   #not the root folder
   else
-    
+
     #if the folder has a slideshow.xml file but no subdirectory (this collection has images but no subcollections)
     if (has_xml_file?(Dir.entries(path)) and !has_directory?(path))
       #logger.debug("has xml, no subdirectory")
-      
+
       #create the collection
       create_collection(path)
-    
+
     #if the folder doesn't have a slideshow.xml file but has subdirectory (this collections doesn't have images but has a subcollection)
     elsif (!has_xml_file?(Dir.entries(path)) and has_directory?(path))
       #logger.debug("no xml, has subdirectory")
-      
+
       #create the collection, get it's pid
       pid = create_collection_no_images(path)
-      
+
       #update the folder name by appending the pid
       new_path = update_folder_name_with_pid(path, pid)
-      
+
       #read the subcollection directory
-      Dir.glob("#{new_path}/*") do |name|   
+      Dir.glob("#{new_path}/*") do |name|
         #if it has a subcollection, read that directory
         if FileTest.directory? name
           read_folder(name)
         end
       end
-    
-    #if the folder has a slideshow.xml file and has subdirectory (this collections has images and has a subcollection) 
+
+    #if the folder has a slideshow.xml file and has subdirectory (this collections has images and has a subcollection)
     elsif (has_xml_file?(Dir.entries(path)) and has_directory?(path))
       #logger.debug("has xml, has subdirectory")
-      
+
       #create the collection, save the pid
       pid = create_collection(path)
-     
+
       #update the folder name by appending the pid
       new_path = update_folder_name_with_pid(path, pid)
-      
+
       #read the subcollection directory
-      Dir.glob("#{new_path}/*") do |name|   
+      Dir.glob("#{new_path}/*") do |name|
         if FileTest.directory? name
           read_folder(name)
         end
       end #end block
-      
+
     end #end if-else
   end # end if-else
 end
@@ -109,7 +111,7 @@ end
 #This method will create a collection that doesn't have any images (it will have subcollections)
 def create_collection_no_images(folder_name)
   logger.debug("create parent_collection: #{folder_name}")
-  
+
   #get collection name from folder name
   begin_index = folder_name.rindex("/")
   if (begin_index.present?)
@@ -117,7 +119,7 @@ def create_collection_no_images(folder_name)
   else
     title = folder_name
   end
-  
+
   #create new collection, update it's metadata and save
   collection = DILCollection.new({:pid=>mint_pid("dil-local")})
   collection.apply_depositor_metadata('mcs680')
@@ -125,7 +127,7 @@ def create_collection_no_images(folder_name)
   #collection.set_collection_type('dil_collection')
   collection.descMetadata.title = title
   collection.save!
-  
+
   #if this collection has a parent collection, get it's pid and update the RELS-EXT in both collections
   if (has_parent_collection?(folder_name))
     parent_pid = get_parent_collection_pid(folder_name)
@@ -133,7 +135,7 @@ def create_collection_no_images(folder_name)
     parent_collection = DILCollection.find(parent_pid)
     parent_collection.insert_member(collection)
   end
-  
+
   #logger.debug("New pid: #{collection.pid}")
   # return the pid of the new collection
   collection.pid
@@ -142,12 +144,12 @@ end
 #This method will create a collection that has images
 def create_collection(filename)
   logger.debug("create collection: #{filename}")
-  
+
   #Use XSLT to extract needed info from MDID export xml
   xml_path = "#{filename}/slideshow.xml"
   xsl_path = "extract_collection_info_import.xsl"
   #new_xml = "#{filename}/collection.xml"
-  
+
   xslt = XML::XSLT.new()
   xslt.xml = xml_path
   xslt.xsl = xsl_path
@@ -168,7 +170,7 @@ def create_collection(filename)
   #collection.set_collection_type('dil_collection')
   collection.descMetadata.title = title
   collection.save!
-  
+
   #if this collection has a parent collection, get it's pid and update the RELS-EXT in both collections
   if (has_parent_collection?(filename))
     parent_pid = get_parent_collection_pid(filename)
@@ -176,13 +178,13 @@ def create_collection(filename)
     parent_collection = DILCollection.find(parent_pid)
     parent_collection.insert_member(collection)
   end
-  
+
   #For each image in the MDID export xml, find it in DIL, and add the image to the collection, update RELS-EXT in image and collection
   image_pid.each do |image|
-    
+
     #Query Solr using the accession number for each image
     dil_pids = ActiveFedora::SolrService.query("search_field_t:Voyager#{image.text} AND object_type_facet:Multiresimage")
-    
+
     #If we have exactly one Solr result
     if (dil_pids.present? and dil_pids.size==1)
       logger.debug("Found DIL image")
@@ -199,16 +201,16 @@ def create_collection(filename)
       logger.debug("Could not find DIL image. Acc. Num: #{image.text}")
     end
   end
-  
+
   #logger.debug("New pid: #{collection.pid}")
   collection.pid
-  
+
 end
 
 # This method checks to see if slideshow.xml is in the list of items in a directory
 def has_xml_file?(entries_array)
   return_value = false
-  
+
   entries_array.each do |entry|
     #logger.debug(entry)
     if (entry.match('slideshow.xml'))
@@ -216,7 +218,7 @@ def has_xml_file?(entries_array)
       return_value = true
     end
   end
-  
+
   return_value
 end
 
@@ -229,7 +231,7 @@ def has_directory?(path)
       return_value = true
     end
   end
-  
+
   return_value
 end
 
