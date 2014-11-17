331f96b81b044e174890f8134d9057052ba30728
Chris Syversen <csyversen@gmail.com>
2014-11-17 11:58:35 -0600

    reorganizing specs, moved login info to support file

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,22 +1,26 @@
 require 'rails_helper'
 
-describe 'home page' do
-  describe 'index' do
+describe 'basic functionality' do
+  describe 'from the home page' do
     before do
       visit '/'
     end
 
-    context "not logging in" do
+    context "and not logged in" do
       it "has a login link" do
         expect(page).to have_content("Login")
       end
 
-
-    end
-
-    context "logging in" do
-      it "should try to log in" do
+      it "can pull up the login page" do
         click_link 'Login'
+        expect(page).to have_content("Sign in with a NetID")
+      end
+
+      it "should log in" do
+        user = FactoryGirl.create(:staff)
+        # this is a method in support/login.rb
+        login(user)
+        expect(page).to have_content("Successfully authorized")
       end
     end
 
