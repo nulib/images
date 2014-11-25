9a8416b9952b11c0bfd16176a08ee5783352ec3e
Paul Clough <p-clough@northwestern.edu>
2014-11-25 16:29:24 -0600

    Add a test to search for creator

---
+++ spec/features/search_creator_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'Search for a creator or keyword' do
+  describe 'after logging in' do
+    before do
+      user = FactoryGirl.create(:staff)
+      login( user )
+      visit( '/' )
+    end
+
+    it "searches for a creator" do
+      fill_in( 'q', '"Wilde, Oscar, 1854-1900"' )
+      click_button( 'submit' )
+      expect( page ).to have_selector( '#documents .listing', count: 3 )
+    end
+  end
+end
