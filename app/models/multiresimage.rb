f09f4d5a04e4c49028b21755b98f8a5a81e19042
Chris Syversen <csyversen@gmail.com>
2014-12-03 17:11:26 -0600

    dynamic path for jp2_deliv_ops_xml

--- app/models/multiresimage.rb
+++ app/models/multiresimage.rb
@@ -239,9 +239,12 @@ class Multiresimage < ActiveFedora::Base
 
   def jp2_deliv_ops_xml( width, height, rel_path, pid )
     rel_path = rel_path.chop if rel_path.end_with?( '/' )
+    path_array = rel_path.split('/')
+    short_path = path_array[4, path_array.length].join('/')
+
     xml = <<-EOF
 <svg:svg xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <svg:image x="0" y="0" height="#{ height }" width="#{ width }" xlink:href="/inu-dil/hydra/test/#{ jp2_img_name }"/>
+  <svg:image x="0" y="0" height="#{ height }" width="#{ width }" xlink:href="/#{short_path}/#{ jp2_img_name }"/>
 </svg:svg>
 EOF
   end
