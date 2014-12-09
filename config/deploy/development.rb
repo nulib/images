b903ff71d7bfdd067baffd45275a2c259be40433
Chris Syversen <csyversen@gmail.com>
2014-12-09 14:15:38 -0600

    adding secrets to shared dir; adding new dev environment

---
+++ config/deploy/development.rb
@@ -0,0 +1,48 @@
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary server in each group
+# is considered to be the first unless any hosts have the primary
+# property set.  Don't declare `role :all`, it's a meta role.
+
+#role :app, %w{deploy@emo-d.library.northwestern.edu}
+#role :web, %w{deploy@emo-d.library.northwestern.edu}
+#role :db,  %w{deploy@emo-d.library.northwestern.edu}
+role :app, %w{deploy@images-d.library.northwestern.edu}
+role :web, %w{deploy@images-d.library.northwestern.edu}
+role :db,  %w{deploy@images-d.library.northwestern.edu}
+
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server definition into the
+# server list. The second argument is a, or duck-types, Hash and is
+# used to set extended properties on the server.
+
+#server 'emo-d.library.northwestern.edu', user: 'deploy', roles: %w{web app db}, my_property: :my_value
+server 'images-d.library.northwestern.edu', user: 'deploy', roles: %w{web app db}, my_property: :my_value
+
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
+#
+# Global options
+# --------------
+#  set :ssh_options, {
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
+#
+# And/or per server (overrides global)
+# ------------------------------------
+# server 'example.com',
+#   user: 'user_name',
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: 'user_name', # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: 'please use keys'
+#   }
