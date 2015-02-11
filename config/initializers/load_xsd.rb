4315c112fc7d98e4703f9cd3f9964f9bc89ab1f6
Chris Syversen <csyversen@gmail.com>
2015-02-11 10:47:04 -0800

    i lied, let's just go with this for right now

--- config/initializers/load_xsd.rb
+++ config/initializers/load_xsd.rb
@@ -1,9 +1,4 @@
 require 'nokogiri'
 require 'open-uri'
 
-begin
-  XSD = Nokogiri::XML::Schema(open("http://www.loc.gov/standards/vracore/vra-strict.xsd").read)
-rescue
-  # Load up a local XSD if for some reason you can't access LOC
-  XSD = Nokogiri::XML::Schema(open("lib/dil/vra-strict.xsd").read)
-end
+XSD = Nokogiri::XML::Schema(open("http://www.loc.gov/standards/vracore/vra-strict.xsd").read)
