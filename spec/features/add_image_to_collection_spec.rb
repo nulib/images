04840d93592939a5c4bc4cbf1ce99bbd85269fab
Jennifer Lindner <jenlindner@gmail.com>
2015-04-07 14:48:18 -0500

    test

--- spec/features/add_image_to_collection_spec.rb
+++ spec/features/add_image_to_collection_spec.rb
@@ -163,6 +163,14 @@ steps 'Logged-in Users can Manage their Groups of Images',  :js => true do
     delete_test_group('Test Group')
   end
 
+  it "won't display 'add an image to a group' unless user is signed in and has groups" do
+    find_link('Creator').click
+    find_link('U.S. G.P.O.').click
+
+    expect(page).to_not have_content("Add to Image Group")
+  end
+
+
   it "lets a user add a subgroup to a group" do
     # drag it onto the test group
     # test that if you click the subgroup it has the correct parent group in its page
