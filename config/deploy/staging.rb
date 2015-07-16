0225b416855d75ffa216af75c92bd2a27b01fb4d
Jennifer Lindner <jenlindner@gmail.com>
2015-07-16 12:20:27 -0500

    different syntax for group selection for staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -23,7 +23,7 @@ set :rbenv_ruby, '2.2.2'
 
 
 #bundle install only staging gems
-set :bundle_with, %w{default staging}.join(' ')
+set :bundle_without, [:development, :test, :production]
 
 # Custom SSH Options
 # ==================
