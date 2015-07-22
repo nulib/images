606447c4d267387582a3f9160043d112f454c197
Jennifer Lindner <jenlindner@gmail.com>
2015-07-22 12:46:10 -0500

    binstubs for each deploy in remote_dev without set again too

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,9 @@ set :linked_dirs, %w{jetty tmp/pids log} #bin log tmp/pids tmp/cache tmp/sockets
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-# set :bundle_flags, "--deployment"
+set :bundle_flags, ""
+set :bundle_without %w{development test staging production}.join(' ')
+set :bundle_binstubs, -> { shared_path.join('bin') }
 
 # rbenv setup
 set :rbenv_type, :user
