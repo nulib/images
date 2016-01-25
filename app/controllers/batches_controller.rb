55b664f926e63af71b577d2582a6bffdff865656
Jennifer Lindner <jenlindner@gmail.com>
2016-01-25 15:08:22 -0600

    it works

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -4,23 +4,19 @@ class BatchesController < ApplicationController
 include DIL::PidMinter
 
   def new
-
   end
 
   def create
-    puts "  hello!!!   "
-
-    xml_files = Dir.glob( "#{DIL_CONFIG['batch_dir']}/#{params[:job_number]}/*.xml" )
-    path = "#{DIL_CONFIG['batch_dir']}/#{params[:job_number]}"
-
+    xml_files = Dir.glob( "#{DIL_CONFIG['batch_dir']}/#{params[:job_number]}/[\d].xml" )
     xml_files.each do |xf|
-
+      xml = nokogiri_doc = Nokogiri::XML(File.read( xf ))
       pid = mint_pid("dil")
-      multiresimage = Multiresimage.new(pid: pid, vra_xml: xf, from_menu: true)
+      multiresimage = Multiresimage.new(pid: pid, vra_xml: xml.to_xml(), from_menu: true)
       multiresimage.save
-      multiresimage.create_datastreams_and_persist_image_files(path)
+      tif_path = File.file?(xf.gsub(/.xml/, '.tiff')) ? xf.gsub(/.xml/, '.tiff') : xf.gsub(/.xml/, '.tif')
+      multiresimage.create_datastreams_and_persist_image_files(tif_path)
     end
-    head :ok
+    render :create
   end
 
 end
