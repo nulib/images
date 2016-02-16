33cca09ea0f870dbe690fcf0e4b9e9ee8f79ddf7
Jennifer Lindner <jenlindner@gmail.com>
2016-02-16 16:37:08 -0600

    using configs for fedora auth

--- config/initializers/riiif_initializer.rb
+++ config/initializers/riiif_initializer.rb
@@ -1,18 +1,22 @@
+
+FEDORA_CONFIG = YAML.load_file(Rails.root.join('config', 'fedora.yml'))[Rails.env]
+
 # Tell RIIIF to get files via HTTP (not from the local disk)
 Riiif::Image.file_resolver = Riiif::HTTPFileResolver.new
-Riiif::Image.file_resolver.basic_auth_credentials = ['fedoraAdmin', 'fedoraAdmin']
+Riiif::Image.file_resolver.basic_auth_credentials = [FEDORA_CONFIG['user'], FEDORA_CONFIG['password']]
 
 # This tells RIIIF how to resolve the identifier to a URI in Fedora
 DATASTREAM = 'DELIV-IMG'
-Riiif::Image.file_resolver.id_to_uri = lambda do |id| 
+Riiif::Image.file_resolver.id_to_uri = lambda do |id|
   connection = ActiveFedora::Base.connection_for_pid(id)
   host = connection.config[:url]
   path = connection.api.datastream_content_url(id, DATASTREAM, {})
+  # logger.info("what the hell, yo?? #{host + '/' + path}")
   host + '/' + path
 end
 
 # In order to return the info.json endpoint, we have to have the full height and width of
-# each image. If you are using hydra-file_characterization, you have the height & width 
+# each image. If you are using hydra-file_characterization, you have the height & width
 # cached in Solr. The following block directs the info_service to return those values:
 HEIGHT_SOLR_FIELD = 'height_isi'
 WIDTH_SOLR_FIELD = 'width_isi'
