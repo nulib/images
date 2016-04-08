a741b220623655c8ea1f78cd5bcca42b67406a64
Jennifer Lindner <jenlindner@gmail.com>
2016-04-08 09:12:01 -0500

    sidekiq configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,7 +40,8 @@ set :bundle_binstubs, -> { shared_path.join('bin') }
 # rbenv setup
 set :rbenv_type, :user
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+#set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+#set :rbenv_map_bins, fetch(:rbenv_map_bins).to_a.concat(%w({rake gem bundle ruby rails}))
 set :rbenv_roles, :all # default value
 
 
