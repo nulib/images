3c231d2bc3507a373aafe32028ab6d76bc908106
Jennifer Lindner <jenlindner@gmail.com>
2016-04-08 09:14:28 -0500

    sidekiq configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,7 +55,7 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       execute :touch, release_path.join('tmp/restart.txt')
-      invoke 'delayed_job:restart'
+    #  invoke 'delayed_job:restart'
     end
   end
 
