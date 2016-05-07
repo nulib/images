a318ade38d1fcbb609493bb85039cd8f25072a7f
Jennifer Lindner <jenlindner@gmail.com>
2016-05-07 16:41:55 -0500

    getting object in info_service

--- config/initializers/riiif_initializer.rb
+++ config/initializers/riiif_initializer.rb
@@ -7,6 +7,7 @@ Riiif::Image.file_resolver.base_path = "#{DIL_CONFIG['jp2_location']}"
 #overwriting info service to add scale factors -- probably not necessary but might be helpful
 Riiif::Image.info_service = lambda do |id, file|
   Rails.cache.fetch(Riiif::Image.cache_key(id, { info: true }), compress: true, expires_in: Riiif::Image.expires_in) do
+    file.info
     {height: file.info.height, width: file.info.width, scale_factors: [1, 2, 4, 8, 16, 32]}
   end
 end
