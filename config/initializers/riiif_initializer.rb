0bbdf684ba6b372b9a726c895acd04e417f2f792
Jennifer Lindner <jenlindner@gmail.com>
2016-05-07 16:56:27 -0500

    not overwriting not worth it

--- config/initializers/riiif_initializer.rb
+++ config/initializers/riiif_initializer.rb
@@ -4,14 +4,6 @@ DIL_CONFIG = YAML.load_file(Rails.root.join('config', 'dil-config.yml'))[Rails.e
 
 Riiif::Image.file_resolver.base_path = "#{DIL_CONFIG['jp2_location']}"
 
-#overwriting info service to add scale factors -- probably not necessary but might be helpful
-Riiif::Image.info_service = lambda do |id, file|
-  Rails.cache.fetch(Riiif::Image.cache_key(id, { info: true }), compress: true, expires_in: Riiif::Image.expires_in) do
-    file.info[scale_factors: [1, 2, 4, 8, 16, 32]]
-  end
-end
-
-
 def logger
   Rails.logger
 end
