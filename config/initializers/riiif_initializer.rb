5da8c19058062d18b4877fdaad934034b8007aa7
Jennifer Lindner <jenlindner@gmail.com>
2016-05-07 16:29:24 -0500

    riiif file resolver working locally

--- config/initializers/riiif_initializer.rb
+++ config/initializers/riiif_initializer.rb
@@ -2,23 +2,13 @@
 FEDORA_CONFIG = YAML.load_file(Rails.root.join('config', 'fedora.yml'))[Rails.env]
 DIL_CONFIG = YAML.load_file(Rails.root.join('config', 'dil-config.yml'))[Rails.env]
 
-Riiif::Image.file_resolver = Riiif::HTTPFileResolver.new
-Riiif::Image.file_resolver.basic_auth_credentials = ["#{FEDORA_CONFIG['user']}", "#{FEDORA_CONFIG['password']}"]
+Riiif::Image.file_resolver.base_path = "#{DIL_CONFIG['jp2_location']}"
 
-
-DATASTREAM = 'DELIV-IMG'
-Riiif::Image.file_resolver.id_to_uri = lambda do |id|
-  connection = ActiveFedora::Base.connection_for_pid(id)
-  host = connection.config[:url]
-  path = connection.api.datastream_content_url(id, DATASTREAM, {})
-  host + '/' + path
-end
-
+#overwriting info service to add scale factors -- probably not necessary but might be helpful
 Riiif::Image.info_service = lambda do |id, file|
-  img = id.gsub(/:/, "-")
-  img_path = "#{DIL_CONFIG['jp2_location']}#{img}.jp2"
-  info = Multiresimage.new.get_image_width_and_height(img_path)
-  { height: info[:height], width: info[:width], scale_factors: [1, 2, 4, 8, 16, 32] }
+  Rails.cache.fetch(Riiif::Image.cache_key(id, { info: true }), compress: true, expires_in: Riiif::Image.expires_in) do
+    {height: image.info.height, width: image.info.width, scale_factors: [1, 2, 4, 8, 16, 32]}
+  end
 end
 
 
