a348c91715c78ba17e1a70d301f8a760a08a8300
bmquinn <brendan-quinn@northwestern.edu>
2016-08-09 13:44:15 -0500

    Clean up Multiresimage feature test

--- spec/features/multiresimage_spec.rb
+++ spec/features/multiresimage_spec.rb
@@ -1,18 +1,7 @@
 require 'capybara/rspec'
 require 'rails_helper'
 
-
 RSpec.feature "Image Viewing", :type => :feature, :js => true do
-  before :all do
-    @driver = Capybara.default_driver
-    visit('http://localhost:3331/users/sign_in')
-    within("#new_user") do
-      fill_in 'username', :with => Rails.application.secrets["test_non_admin_id"]
-      fill_in 'password', :with => Rails.application.secrets["test_non_admin_password"]
-    end
-    click_button('signIn')
-  end
-
   scenario 'the multiresimage info.json is in the OSD-generated html' do
     visit "http://localhost:3331/multiresimages/inu:dil-af3c7e97-8fee-4a3d-8584-913fd3089c92"
     expect(page).to have_css 'div.openseadragon-container'
