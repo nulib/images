57d5897bb33721c350ce19b14d5ccbf812ec5610
bmquinn <brendan-quinn@northwestern.edu>
2016-08-09 13:32:19 -0500

    Use a different fixture image in feature test

--- spec/features/multiresimage_spec.rb
+++ spec/features/multiresimage_spec.rb
@@ -14,8 +14,8 @@ RSpec.feature "Image Viewing", :type => :feature, :js => true do
   end
 
   scenario 'the multiresimage info.json is in the OSD-generated html' do
-    visit "http://localhost:3331/multiresimages/inu:dil-cffada80-57f3-4d98-a0ee-e73048943f90"
+    visit "http://localhost:3331/multiresimages/inu:dil-af3c7e97-8fee-4a3d-8584-913fd3089c92"
     expect(page).to have_css 'div.openseadragon-container'
-    expect(page.html).to include('<source src="/image-service/inu-dil-cffada80-57f3-4d98-a0ee-e73048943f90/info.json" media="openseadragon" />')
+    expect(page.html).to include('<source src="/image-service/inu-dil-af3c7e97-8fee-4a3d-8584-913fd3089c92/info.json" media="openseadragon" />')
   end
 end
