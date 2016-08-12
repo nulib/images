6b36f09fe6f6786cf49bcf9292e690dfb1c037dd
Brendan Quinn <bmq449@dhcp-129-105-19-223.library.northwestern.edu>
2016-08-12 15:22:11 -0500

    Remove unused AdminPolicy models, controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -71,8 +71,6 @@ Rails.application.routes.draw do
     resources :users, :only=>[:create, :edit, :destroy]
   end
 
-
-
   resources :batches
   # The routes below aren't resourceful, but I'm not sure if anything outside of the application is referring to them
   # so I don't want to refactor them into resourceful routes. I created placeholders for them above though - CS 11-18-2014
