a6e0526470b598cf911ca5c7c8b033c8512c7942
Chris Syversen <csyversen@gmail.com>
2016-08-19 15:41:13 -0500

    fairly safe changes

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -5,11 +5,11 @@
 Mime::Type.register_alias "text/html", :textile
 Mime::Type.register_alias "text/html", :inline
 
-Mime::Type.register_alias "text/plain", :refworks_marc_txt
-Mime::Type.register_alias "text/plain", :openurl_kev
-Mime::Type.register "application/x-endnote-refer", :endnote
-Mime::Type.register "application/marc", :marc
-Mime::Type.register "application/marcxml+xml", :marcxml,
+#Mime::Type.register_alias "text/plain", :refworks_marc_txt
+#Mime::Type.register_alias "text/plain", :openurl_kev
+#Mime::Type.register "application/x-endnote-refer", :endnote
+#Mime::Type.register "application/marc", :marc
+#Mime::Type.register "application/marcxml+xml", :marcxml,
       ["application/x-marc+xml", "application/x-marcxml+xml",
        "application/marc+xml"]
 
