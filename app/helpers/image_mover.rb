26fa63f20c446d4745686fee5581103320b1eec1
Karen Didrickson <karendid@gmail.com>
2016-08-22 14:34:11 -0500

    Fix tif path variable argument in ImageMover

--- app/helpers/image_mover.rb
+++ app/helpers/image_mover.rb
@@ -1,7 +1,7 @@
 module ImageMover
   def self.move_img_to_repo(source_path)
     if Rails.env.staging? || Rails.env.production?
-      raise "Wrong image format, not tif" unless File.extname(img_name) == ".tif"
+      raise "Wrong image format, not tif" unless File.extname(source_path) == ".tif"
       destination_path = "#{DIL_CONFIG['repo_location']}#{ File.basename(source_path) }"
       FileUtils.mv(source_path, destination_path)
     end
