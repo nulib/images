7e518e07c3f8b84979cf465a9af04e308373fad8
Chris Syversen <csyversen@gmail.com>
2016-08-29 15:06:52 -0500

    removing load_dil_configs because they are getting loaded elsewhere, and removing duplicate mime type registrations

--- config/initializers/load_dil_configs.rb
+++
@@ -1,4 +0,0 @@
-#DIL_CONFIG = YAML.load_file(Rails.root.join('config', 'dil-config.yml'))[Rails.env]
-#Added per vulnerability discovered https://groups.google.com/forum/?fromgroups=#!topic/rubyonrails-security/61bkgvnSGTQ
-#ActiveSupport::XmlMini::PARSING.delete("symbol")
-#ActiveSupport::XmlMini::PARSING.delete("yaml")
