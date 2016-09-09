5ada9e4be1a58f91efe855bfc4a817340d46d17b
Chris Syversen <csyversen@gmail.com>
2016-09-09 12:57:19 -0500

    adding in logging for image mover

--- app/helpers/image_mover.rb
+++ app/helpers/image_mover.rb
@@ -3,7 +3,14 @@ module ImageMover
     if Rails.env.staging? || Rails.env.production?
       raise "Wrong image format, not tif" unless File.extname(source_path) == ".tif"
       destination_path = "#{DIL_CONFIG['repo_location']}#{ File.basename(source_path) }"
+      logger.info("repo_location exist? #{File.exist?(DIL_CONFIG['repo_location'])}")
+      logger.info("destination_path: #{destination_path}")
+
+      logger.info("does the source file exist??? : #{File.exist?(source_path)}")
+      logger.info("about to move tif to repo...")
       FileUtils.mv(source_path, destination_path)
+      logger.info("after move has been called")
+      logger.info("is the tif at the destination? : #{File.exist?(destination_path)}")
     end
   end
 end
