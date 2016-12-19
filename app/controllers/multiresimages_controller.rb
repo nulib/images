5ecbcc1f3c6d3c125b139019969d9b798e39ee3a
Karen Didrickson <karendid@gmail.com>
2016-12-19 10:51:22 -0600

    Remove additional button

--- app/controllers/multiresimages_controller.rb
+++ app/controllers/multiresimages_controller.rb
@@ -145,24 +145,4 @@ class MultiresimagesController < ApplicationController
     end
   end
 
-  def delivery_image_proxy
-    multiresimage = Multiresimage.find(params[:id])
-    if current_user.admin?
-      if multiresimage.DELIV_IMG.dsLocation == nil
-        flash[:error] = "No JP2 location (path) associated with this image."
-        redirect_to multiresimage_path
-      else
-        begin 
-          filename = "download.jp2"
-          send_data(multiresimage.DELIV_IMG.content, :type=>'image/jp2', :filename=>filename) unless multiresimage.DELIV_IMG.content.nil? # throws Rubydora::FedoraInvalidRequest
-        rescue => e
-          flash[:error] = "Problem retrieving jp2 file for this image: #{e.class}, #{e.message}"
-          redirect_to multiresimage_path
-        end
-      end
-    else
-      render :nothing => true
-    end
-  end
-
 end
