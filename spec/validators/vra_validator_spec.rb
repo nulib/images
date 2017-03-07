a433bdedfacafad47bf53925e6902b1e72989300
bmquinn <brendan-quinn@northwestern.edu>
2017-03-07 16:26:42 -0600

    refactor VraValidator specs, fix a few more

--- spec/validators/vra_validator_spec.rb
+++ spec/validators/vra_validator_spec.rb
@@ -13,27 +13,31 @@ RSpec.describe VraValidator do
     expect(@dummy_class).to respond_to(:valid_vra?)
   end
 
-  it 'makes #validate_vra available' do
-    expect(@dummy_class).to respond_to(:validate_vra)
+  it 'makes #vra_errors? available' do
+    expect(@dummy_class).to respond_to(:vra_errors?)
   end
 
-  context 'validation' do
-    describe 'with invalid vra' do
-      it 'should fail' do
-        expect(@dummy_class.valid_vra?(File.open(Rails.root.join('spec', 'fixtures', 'vra_image_minimal_invalid.xml')).read)).to be_falsey
-      end
+  describe '#get_validation_errors' do
+    it 'returns an empty array for valid vra' do
+      expect(@dummy_class.get_validation_errors(File.open(Rails.root.join('spec', 'fixtures', 'vra_image_minimal.xml')).read)).to be_empty
     end
 
-    describe 'with valid vra' do
-      it 'should pass' do
-        expect(@dummy_class.validate_vra(File.open(Rails.root.join('spec', 'fixtures', 'vra_image_minimal.xml')).read)).to be_truthy
-      end
+    it 'returns an array of errors for invalid vra' do
+      expect(@dummy_class.get_validation_errors(File.open(Rails.root.join('spec', 'fixtures', 'vra_image_minimal_invalid.xml')).read)).to eq ["Validation error: Element '{http://www.vraweb.org/vracore4.htm}earliestDate': [facet 'pattern'] The value '' is not accepted by the pattern 'present|(-)*[0-9]{1,12}(-[0-9]{2}(-[0-9]{2})*)*'.\n", "Validation error: Element '{http://www.vraweb.org/vracore4.htm}earliestDate': '' is not a valid value of the atomic type '{http://www.vraweb.org/vracore4.htm}dateValueType'.\n"]
+    end
+  end
+
+  describe '#vra_errors?' do
+    it 'returns true for invalid vra' do
+      expect(@dummy_class.vra_errors?(File.open(Rails.root.join('spec', 'fixtures', 'vra_image_minimal_invalid.xml')).read)).to be_truthy
     end
 
-    describe 'valid vra except for pids' do
-      it 'should pass' do
-        expect(@dummy_class.validate_vra(File.open(Rails.root.join('spec', 'fixtures', 'vra_image_with_pids.xml')).read)).to be_truthy
-      end
+    it 'returns false for valid vra' do
+      expect(@dummy_class.vra_errors?(File.open(Rails.root.join('spec', 'fixtures', 'vra_image_minimal.xml')).read)).to be_falsey
+    end
+
+    it 'returns false for valid vra that has a locationSet refid containing an inu:dil pid' do
+      expect(@dummy_class.vra_errors?(File.open(Rails.root.join('spec', 'fixtures', 'vra_image_with_pids.xml')).read)).to be_falsey
     end
   end
 end
